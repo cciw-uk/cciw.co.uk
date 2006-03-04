f45c09f940b9524882858132be48e5cc6f724102
luke <devnull@localhost>
2006-03-04 19:58:15 +0000

    - Updated settings for middleware - Switched to postgres

--- devel.py
+++ devel.py
@@ -1,5 +1,6 @@
 # import this to enable interactive use of django and the cciw app
 import sys
 import os
+#sys.path = sys.path + ['/home/luke/devel/django/magic-removal', '/home/httpd/www.cciw.co.uk/django/',]
 sys.path = sys.path + ['/home/httpd/www.cciw.co.uk/django/','/home/httpd/www.cciw.co.uk/django_src/']
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cciw.settings'
