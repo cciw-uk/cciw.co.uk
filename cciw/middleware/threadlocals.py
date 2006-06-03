254834904ebd6924ccbea1bda094e480556714e2
luke <devnull@localhost>
2006-06-03 22:29:32 +0000

    Added a method for setting threadlocal user

--- cciw/middleware/threadlocals.py
+++ cciw/middleware/threadlocals.py
@@ -6,6 +6,9 @@ _thread_locals = threading.local()
 
 def get_current_user():
     return getattr(_thread_locals, 'user', None)
+    
+def set_current_user(user):
+    _thread_locals.user = user
 
 def get_current_member():
     """Returns the currently logged in member, or None"""
