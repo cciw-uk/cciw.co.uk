ab42f7df4f117b7e26e32968d6755fd3059132b2
luke <devnull@localhost>
2006-06-03 13:47:10 +0000

    Fixed path.

--- devel.py
+++ devel.py
@@ -2,5 +2,5 @@
 import sys
 import os
 #sys.path = sys.path + ['/home/luke/devel/django/magic-removal', '/home/httpd/www.cciw.co.uk/django/',]
-sys.path = sys.path + ['/home/httpd/www.cciw.co.uk/django/','/home/httpd/www.cciw.co.uk/django_src/']
+sys.path = sys.path + ['/home/luke/httpd/www.cciw.co.uk/django/','/home/luke/httpd/www.cciw.co.uk/django_src/']
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cciw.settings_calvin'
