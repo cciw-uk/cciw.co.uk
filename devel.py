1734688b8c2a55b1cb6248c07ee357a3e201240f
luke <devnull@localhost>
2007-04-02 22:17:18 +0000

    Updated paths for misc_src files

--- devel.py
+++ devel.py
@@ -2,5 +2,5 @@
 import sys
 import os
 #sys.path = sys.path + ['/home/luke/devel/django/magic-removal', '/home/httpd/www.cciw.co.uk/django/',]
-sys.path = sys.path + ['/home/luke/httpd/www.cciw.co.uk/django/','/home/luke/httpd/www.cciw.co.uk/django_src/', '/home/luke/httpd/www.cciw.co.uk/misc_src/']
+sys.path = sys.path + ['/home/luke/httpd/www.cciw.co.uk/django/','/home/luke/httpd/www.cciw.co.uk/django_src/', '/home/luke/local/lib/python2.4/site-packages/']
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cciw.settings_calvin'
