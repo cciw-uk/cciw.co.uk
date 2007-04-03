e5863f6d73513fff1b63cda1696f5e114e9b9377
luke <devnull@localhost>
2007-04-03 19:32:05 +0000

    Updated settings file for testing env, with some refactorings

--- cciw/settings_calvin.py
+++ cciw/settings_calvin.py
@@ -10,9 +10,12 @@ DATABASE_USER = 'cciw'
 DATABASE_PASSWORD = 'foo' 
 DATABASE_HOST = ''        # Set to empty string for localhost. Not used with sqlite3.
 
+
+basedir = r'/home/luke/httpd/www.cciw.co.uk/django'
+
 # Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/luke/httpd/www.cciw.co.uk/django/media/'
+
+MEDIA_ROOT =  basedir + '/media/'
 
 # URL that handles the media served from MEDIA_ROOT.
 # Example: "http://media.lawrence.com"
@@ -23,9 +26,9 @@ SPECIAL_MEDIA_URL = 'http://cciw_django_local/sp_media/'
 MIDDLEWARE_CLASSES = (
     "django.middleware.common.CommonMiddleware",
 #    "django.middleware.cache.CacheMiddleware",
-#    "django.middleware.gzip.GZipMiddleware",
+#    "django.middleware.gzip.GZipMiddleware", # interferes with testing
     "lukeplant_me_uk.django.validator.middleware.ValidatorMiddleware",
-    "django.contrib.csrf.middleware.CsrfMiddleware",
+#    "django.contrib.csrf.middleware.CsrfMiddleware", # interferes with testing
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "cciw.middleware.threadlocals.ThreadLocals",
@@ -36,15 +39,16 @@ INSTALLED_APPS = INSTALLED_APPS + (
 )
 
 TEMPLATE_DIRS = (
-    r'/home/luke/httpd/www.cciw.co.uk/django/templates/',
+    basedir + r'/templates/',
     r'/home/luke/httpd/www.cciw.co.uk/django_src/django/contrib/admin/templates/',
-    r'/home/luke/httpd/www.cciw.co.uk/django/lukeplant_me_uk/django/validator/templates/',
-    r'/home/luke/httpd/www.cciw.co.uk/django/lukeplant_me_uk/django/tagging/templates/',
+    basedir + r'/lukeplant_me_uk/django/validator/templates/',
+    basedir + r'/lukeplant_me_uk/django/tagging/templates/',
 )
 
 FIXTURE_DIRS = [
-    r'/home/luke/httpd/www.cciw.co.uk/django/cciw/cciwmain/fixtures/'
+    basedir + r'/cciw/cciwmain/fixtures/'
 ]
+TEST_DIR = basedir + r'/cciw/cciwmain/tests/'
 
 ADMIN_MEDIA_PREFIX = '/admin_media/'
 
