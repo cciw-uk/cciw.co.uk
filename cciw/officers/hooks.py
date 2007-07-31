ed0854d79d894e4acd24b62922eb57628c5d414d
luke <devnull@localhost>
2007-07-31 15:50:57 +0000

    Small refactor, and changed emails so that the confirmation email to officer is not sent if it was someone else (e.g. an admin) who updated their application.

--- cciw/officers/hooks.py
+++ cciw/officers/hooks.py
@@ -5,6 +5,7 @@ from cciw.officers.applications import application_to_text, application_to_rtf, 
 from django.dispatch import dispatcher
 from cciw.officers.email_utils import send_mail_with_attachments, formatted_email
 from django.conf import settings
+import cciw.middleware.threadlocals as threadlocals
 
 def send_application_emails(application=None):
     if not application.finished:
@@ -27,16 +28,16 @@ def send_application_emails(application=None):
     rtf_attachment = (application_rtf_filename(application), application_rtf, 'text/rtf')
     
     if len(leader_emails) > 0:
-        msg = \
-"""The following application form has been submitted via the
-CCIW website.  It is also attached to this email as an RTF file.
+        send_leader_email(leader_emails, application, application_text, rtf_attachment)
 
-""" + application_text
-    
-        subject = "CCIW application form from %s" % application.full_name
-    
-        send_mail_with_attachments(subject, msg, settings.SERVER_EMAIL,
-                                   leader_emails, attachments=[rtf_attachment])
+    # If an admin user corrected an application, we don't send the user a copy
+    # (usually they just get the year of the camp wrong(!))
+    user = threadlocals.get_current_user()
+    if user == application.officer:
+        send_officer_email(application.officer, application, application_text, rtf_attachment)
+
+def send_officer_email(officer, application, application_text, rtf_attachment):
+    subject = "CCIW application form submitted"
 
     # Email to the officer
     user_email = formatted_email(application.officer)
@@ -47,11 +48,23 @@ For your records, here is a copy of the application you have submitted
 to CCIW. It is also attached to this email as an RTF file.
 
 """ % application.officer.first_name) + application_text
-    subject = "CCIW application form submitted"
 
     if user_email is not None:
         send_mail_with_attachments(subject, user_msg, settings.SERVER_EMAIL,
                                    [user_email], attachments=[rtf_attachment])
+
+
+
+def send_leader_email(leader_emails, application, application_text, rtf_attachment):
+    subject = "CCIW application form from %s" % application.full_name
+    body = \
+"""The following application form has been submitted via the
+CCIW website.  It is also attached to this email as an RTF file.
+
+""" + application_text
+        
+    send_mail_with_attachments(subject, body, settings.SERVER_EMAIL,
+                               leader_emails, attachments=[rtf_attachment])
 
 
 dispatcher.connect(send_application_emails, signal=signals.application_saved)
