5779f5f787f8198085d73719b33dfbcc70b08c8f
Luke Plant <L.Plant.98@cantab.net>
2007-12-29 01:24:41 +0000

    Removed unused file

--- devel.py
+++
@@ -1,6 +0,0 @@
-# import this to enable interactive use of django and the cciw app
-import sys
-import os
-#sys.path = sys.path + ['/home/luke/devel/django/magic-removal', '/home/httpd/www.cciw.co.uk/django/',]
-sys.path = sys.path + ['/home/luke/httpd/www.cciw.co.uk/django/','/home/luke/httpd/www.cciw.co.uk/django_src/', '/home/luke/local/lib/python2.4/site-packages/']
-os.environ['DJANGO_SETTINGS_MODULE'] = 'cciw.settings_calvin'
