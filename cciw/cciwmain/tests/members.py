4d36f7cb96adbffcaeb2c246c91e90578c49542b
Luke Plant <L.Plant.98@cantab.net>
2008-03-25 20:53:09 +0000

    Added twill tests for the rest of the signup procedure

--- cciw/cciwmain/tests/members.py
+++ cciw/cciwmain/tests/members.py
@@ -1,3 +1,8 @@
+import twill
+from twill import commands as tc
+from twill.shell import TwillCommandLoop
+from StringIO import StringIO
+
 from client import CciwClient
 from django.test import TestCase
 from django.conf import settings
@@ -7,6 +12,8 @@ from django.core import mail
 from cciw.cciwmain.models import Member
 import cciw.cciwmain.views.members
 import cciw.cciwmain.decorators
+
+from cciw.cciwmain.tests.twillhelpers import twill_setup, twill_teardown, make_twill_url
 
 import os
 import glob
@@ -25,6 +32,7 @@ TEST_POLL_CREATOR_PASSWORD = 'password'
 
 NEW_MEMBER_EMAIL='new_member@test.com'
 NEW_MEMBER_USERNAME='new_member'
+NEW_MEMBER_PASSWORD='mypassword'
 
 MEMBER_ADMIN_URL = reverse("cciwmain.memberadmin.preferences")
 MEMBER_SIGNUP = reverse("cciwmain.memberadmin.signup")
@@ -106,12 +114,10 @@ class MemberAdmin(TestCase):
 
     def tearDown(self):
         self._remove_member_icons()
-        pass
 
     def _remove_member_icons(self):
         for f in glob.glob("%s/%s/%s" % (settings.MEDIA_ROOT, settings.MEMBER_ICON_PATH, self.member.user_name + ".*")):
             os.unlink(f)
-
 
 def url_to_path_and_query(url):
     scheme, netloc, path, params, query, fragment = urlparse.urlparse(url)    
@@ -126,6 +132,10 @@ class MemberSignup(TestCase):
 
     def setUp(self):
         self.client = CciwClient()
+        twill_setup()
+
+    def tearDown(self):
+        twill_teardown()
 
     def test_existing_email(self):
         post_data = dict(submit_email='Submit', email=TEST_MEMBER_EMAIL)
@@ -136,7 +146,6 @@ class MemberSignup(TestCase):
                      "Signing up should not allow an existing email to be reused")
         self.assertEqual(len(mail.outbox), 0)
 
-
     def _test_signup_send_email_part1(self):
         post_data = dict(submit_email='Submit', email=NEW_MEMBER_EMAIL)
         response = self.client.post(MEMBER_SIGNUP, data=post_data)
@@ -145,42 +154,94 @@ class MemberSignup(TestCase):
         self.assert_("an e-mail has been sent" in response.content,
                      "An message saying that an email has been sent should be seen")
         self.assertEqual(len(mail.outbox), 1, "An email should be sent")
-        
 
     def _read_signup_email(self, email):
         # read the email, and follow the link
-        url = re.search("http://.*/signup/.*\w", email.body)
-        self.assert_(url is not None, "No URL found in sent email")
-        path, querydata = url_to_path_and_query(url.group())
-        return path, querydata
+        urlmatch = re.search("http://.*/signup/.*\w", email.body)
+        self.assert_(urlmatch is not None, "No URL found in sent email")
+        url = urlmatch.group()
+        self.assert_("http://www.cciw.co.uk/" in url)
+        path, querydata = url_to_path_and_query(url)
+        return url, path, querydata
 
     def _follow_email_url(self, path, querydata):
         response = self.client.get(path, querydata)
         self.failUnlessEqual(response.status_code, 200)
         return response
-        
+
     def test_signup_send_email(self):
         self._test_signup_send_email_part1()
-        path, querydata = self._read_signup_email(mail.outbox[0])
+        url, path, querydata = self._read_signup_email(mail.outbox[0])
 
     def test_signup_complete_correct(self):
         self._test_signup_send_email_part1()
-        path, querydata = self._read_signup_email(mail.outbox[0])
-        response = self._follow_email_url(path, querydata)
-        self.assert_("Error" not in response.content)
-        # Fill out the form
-        # TODO - test the rest of the process
+        url, path, querydata = self._read_signup_email(mail.outbox[0])
+        local_url = make_twill_url(url)
+        twill.set_output(StringIO())
+        tc.go(local_url)
+        tc.notfind("Error")
+        tc.fv('1', 'user_name', NEW_MEMBER_USERNAME)
+        tc.fv('1', 'password1', NEW_MEMBER_PASSWORD)
+        tc.fv('1', 'password2', NEW_MEMBER_PASSWORD)
+        tc.submit()
+        self._twill_assert_finished()
+
+    def _twill_assert_finished(self):
+        tc.find("Finished!")
+        tc.find("Your account has been created")
+
+    def test_signup_complete_bad_password(self):
+        self._test_signup_send_email_part1()
+        url, path, querydata = self._read_signup_email(mail.outbox[0])
+        local_url = make_twill_url(url)
+        twill.set_output(StringIO())
+        tc.go(local_url)
+        tc.notfind("Error")
+        tc.fv('1', 'user_name', NEW_MEMBER_USERNAME)
+        tc.fv('1', 'password1', NEW_MEMBER_PASSWORD)
+        tc.fv('1', 'password2', NEW_MEMBER_PASSWORD + "x")
+        tc.submit()
+        tc.find("Error")
+
+        # Correct it, without setting user_name
+        tc.fv('1', 'password1', NEW_MEMBER_PASSWORD)
+        tc.fv('1', 'password2', NEW_MEMBER_PASSWORD)
+        tc.submit()
+        self._twill_assert_finished()
+
+    def test_signup_complete_bad_username(self):
+        self._test_signup_send_email_part1()
+        url, path, querydata = self._read_signup_email(mail.outbox[0])
+        local_url = make_twill_url(url)
+        twill.set_output(StringIO())
+        tc.go(local_url)
+        tc.notfind("Error")
+        tc.fv('1', 'user_name', TEST_MEMBER_USERNAME)
+        tc.fv('1', 'password1', NEW_MEMBER_PASSWORD)
+        tc.fv('1', 'password2', NEW_MEMBER_PASSWORD)
+        tc.submit()
+        tc.find("Error")
 
     def test_signup_incorrect_hash(self):
         self._test_signup_send_email_part1()
-        path, querydata = self._read_signup_email(mail.outbox[0])
+        url, path, querydata = self._read_signup_email(mail.outbox[0])
         querydata['h'] = querydata['h'] + "x"
         response = self._follow_email_url(path, querydata)
         self.assert_("Error" in response.content, "Error should be reported if the hash is incorrect")
 
     def test_signup_incorrect_email(self):
         self._test_signup_send_email_part1()
-        path, querydata = self._read_signup_email(mail.outbox[0])
+        url, path, querydata = self._read_signup_email(mail.outbox[0])
         querydata['email'] = querydata['email'] + "x"
         response = self._follow_email_url(path, querydata)
         self.assert_("Error" in response.content, "Error should be reported if the email is incorrect")
+
+class MemberEmail(TestCase):
+    """Tests for changing email address"""
+    
+    fixtures=['basic.yaml','test_members.yaml']
+    def setUp(self):
+        self.client = CciwClient()
+        self.client.member_login(TEST_MEMBER_USERNAME, TEST_MEMBER_PASSWORD)
+        self.member = Member.objects.get(user_name=TEST_MEMBER_USERNAME)
+
---
+++ cciw/cciwmain/tests/twillhelpers.py
@@ -0,0 +1,20 @@
+
+import twill
+from django.core.servers.basehttp import AdminMediaHandler
+from django.core.handlers.wsgi import WSGIHandler
+
+def make_twill_url(url):
+    return url.replace("http://www.cciw.co.uk/", "http://127.0.0.1:8080/")
+
+def twill_setup():
+    app = AdminMediaHandler(WSGIHandler())
+    twill.add_wsgi_intercept("127.0.0.1", 8080, lambda: app)
+
+def twill_teardown():
+    twill.remove_wsgi_intercept('127.0.0.1', 8080)
+
+# Twill snippets 
+# To interactively continue, finish test with this:
+#cmd = TwillCommandLoop()
+#cmd.cmdloop()
+# And make sure that twill.set_output(...) is commented out
