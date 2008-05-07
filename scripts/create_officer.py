3d49d917178b91ea31196e3a4ccd334f6b47ce81
Luke Plant <L.Plant.98@cantab.net>
2008-05-07 23:12:43 +0000

    Updated python paths for script.

--- scripts/create_officer.py
+++ scripts/create_officer.py
@@ -9,7 +9,7 @@ hostname = socket.gethostname()
 
 if hostname == 'calvin':
     sys.path = sys.path + ['/home/luke/httpd/www.cciw.co.uk/current_src/','/home/luke/httpd/www.cciw.co.uk/django_src/', 
-      '/home/luke/local/lib/python2.5/site-packages/', '/home/luke/devel/python/']    
+      '/home/luke/local/lib/python2.4/site-packages/', '/home/luke/devel/python/luke']    
     os.environ['DJANGO_SETTINGS_MODULE'] = 'cciw.settings_calvin'
 else:
     sys.path = sys.path + ['/home2/cciw/webapps/django_app/', '/home2/cciw/src/django-mr/', '/home2/cciw/src/misc/']
