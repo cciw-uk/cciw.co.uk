84623c8e6d6a71954b6a39fe2ee063dfc14b0143
Luke Plant <L.Plant.98@cantab.net>
2008-06-24 15:43:15 +0000

    Removed unused code

--- cciw/officers/views.py
+++ cciw/officers/views.py
@@ -2,25 +2,17 @@ from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
 from django.shortcuts import render_to_response
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib.admin.views.main import add_stage, render_change_form
-from django.contrib.admin.views.main import unquote, quote, get_text_list
-from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
-from django import forms, template
+from django import template
 from django.db import models
 from django.conf import settings
 from django.http import Http404, HttpResponseRedirect, HttpResponse
-from django.utils.encoding import force_unicode, smart_str
 import re
-
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
-
 from cciw.cciwmain.utils import all, StandardReprMixin
-
 from cciw.officers.models import Application, Reference
 from cciw.cciwmain.models import Person, Camp
-
 from cciw.cciwmain import common
 from django.views.decorators.cache import never_cache
 from cciw.officers.applications import application_to_text, application_to_rtf, application_rtf_filename, application_txt_filename
@@ -112,130 +104,6 @@ def index(request):
         pass
 
     return render_to_response('cciw/officers/index.html', context_instance=context)
-
-
-# /admin/officers/application/add/
-@staff_member_required
-def add_application(request):
-    return add_stage(request, "officers", "application", show_delete=False, 
-        form_url='', post_url="/officers/", post_url_continue='../%s/')
-
-
-# /admin/officers/application/%s/
-
-# Copied straight from django.contrib.admin.views.main,
-# with small mods 
-from django.utils.translation import gettext as _ 
-
-@staff_member_required
-@never_cache
-def change_application(request, object_id):
-    app_label, model_name = 'officers', 'application'
-    model = models.get_model(app_label, model_name)
-    object_id = unquote(object_id)
-    if model is None:
-        raise Http404, "App %r, model %r, not found" % (app_label, model_name)
-    opts = model._meta
-
-    # CHANGES
-    user = request.user
-    if not request.user.has_perm(app_label + '.' + opts.get_change_permission()):
-        try:
-            application = Application.objects.get(pk=object_id)
-        except Application.DoesNotExist:
-            raise Http404
-        # only allow them to edit if they created it
-        if application.officer_id != user.id:
-            raise PermissionDenied
-
-    if request.POST and request.POST.has_key("_saveasnew"):
-        return add_application(request, app_label, model_name, form_url='../../add/')
-
-    try:
-        manipulator = model.ChangeManipulator(object_id)
-    except ObjectDoesNotExist:
-        raise Http404
-
-    if request.method == 'POST':
-        new_data = request.POST.copy()
-
-        if opts.has_field_type(models.FileField):
-            new_data.update(request.FILES)
-
-        errors = manipulator.get_validation_errors(new_data)
-        manipulator.do_html2python(new_data)
-
-        if not errors:
-            new_object = manipulator.save(new_data)
-            pk_value = new_object._get_pk_val()
-
-            # Construct the change message.
-            change_message = []
-            if manipulator.fields_added:
-                change_message.append(_('Added %s.') % get_text_list(manipulator.fields_added, _('and')))
-            if manipulator.fields_changed:
-                change_message.append(_('Changed %s.') % get_text_list(manipulator.fields_changed, _('and')))
-            if manipulator.fields_deleted:
-                change_message.append(_('Deleted %s.') % get_text_list(manipulator.fields_deleted, _('and')))
-            change_message = ' '.join(change_message)
-            if not change_message:
-                change_message = _('No fields changed.')
-            LogEntry.objects.log_action(request.user.id, ContentType.objects.get_for_model(model).id, pk_value, force_unicode(new_object), CHANGE, change_message)
-
-            msg = _('The %(name)s "%(obj)s" was changed successfully.') % {'name': opts.verbose_name, 'obj': new_object}
-            if request.POST.has_key("_continue"):
-                request.user.message_set.create(message=msg + ' ' + _("You may edit it again below."))
-                if request.REQUEST.has_key('_popup'):
-                    return HttpResponseRedirect(request.path + "?_popup=1")
-                else:
-                    return HttpResponseRedirect(request.path)
-            elif request.POST.has_key("_saveasnew"):
-                request.user.message_set.create(message=_('The %(name)s "%(obj)s" was added successfully. You may edit it again below.') % {'name': opts.verbose_name, 'obj': new_object})
-                return HttpResponseRedirect("../%s/" % pk_value)
-            elif request.POST.has_key("_addanother"):
-                request.user.message_set.create(message=msg + ' ' + (_("You may add another %s below.") % opts.verbose_name))
-                return HttpResponseRedirect("../add/")
-            else:
-                request.user.message_set.create(message=msg)
-                return HttpResponseRedirect("/officers/")
-    else:
-        # Populate new_data with a "flattened" version of the current data.
-        new_data = manipulator.flatten_data()
-
-        # TODO: do this in flatten_data...
-        # If the object has ordered objects on its admin page, get the existing
-        # order and flatten it into a comma-separated list of IDs.
-
-        id_order_list = []
-        for rel_obj in opts.get_ordered_objects():
-            id_order_list.extend(getattr(manipulator.original_object, 'get_%s_order' % rel_obj.object_name.lower())())
-        if id_order_list:
-            new_data['order_'] = ','.join(map(str, id_order_list))
-        errors = {}
-
-    # Populate the FormWrapper.
-    form = forms.FormWrapper(manipulator, new_data, errors)
-    form.original = manipulator.original_object
-    form.order_objects = []
-
-    #TODO Should be done in flatten_data  / FormWrapper construction
-    for related in opts.get_followed_related_objects():
-        wrt = related.opts.order_with_respect_to
-        if wrt and wrt.rel and wrt.rel.to == opts:
-            func = getattr(manipulator.original_object, 'get_%s_list' %
-                    related.get_accessor_name())
-            orig_list = func()
-            form.order_objects.extend(orig_list)
-
-    c = template.RequestContext(request, {
-        'title': _('Change %s') % opts.verbose_name,
-        'form': form,
-        'object_id': object_id,
-        'original': manipulator.original_object,
-        'is_popup': request.REQUEST.has_key('_popup'),
-    })
-    return render_change_form(model, manipulator, c, change=True)
-
 
 @staff_member_required
 def view_application(request):
--- cciw/urls.py
+++ cciw/urls.py
@@ -6,8 +6,6 @@ handler404 = 'cciw.cciwmain.views.handler404'
 
 urlpatterns = patterns('',
     # Override the admin for some views:
-#    (r'^admin/officers/application/add/$', 'cciw.officers.views.add_application'),
-#    (r'^admin/officers/application/([^/]+)/$', 'cciw.officers.views.change_application'),
     (r'^admin/password_reset/$', 'cciw.officers.views.password_reset'),
     (r'^admin/password_reset/done/$',  'cciw.officers.views.password_reset_done'),
     (r'^admin/password_reset/confirm/$', 'cciw.officers.views.password_reset_confirm'),
