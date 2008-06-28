7ca02b7e19ddb9041b1a2f154ce06db9d4aef920
Luke Plant <L.Plant.98@cantab.net>
2008-06-28 21:59:42 +0000

    Sorted imports

--- cciw/officers/views.py
+++ cciw/officers/views.py
@@ -1,27 +1,27 @@
-from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
-from django.shortcuts import render_to_response, get_object_or_404
-from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.auth.decorators import user_passes_test
-from django import template
-from django.db import models
-from django.conf import settings
-from django.http import Http404, HttpResponseRedirect, HttpResponse
-from django.utils.translation import ugettext_lazy as _
-from django import newforms as forms
-from django.core.urlresolvers import reverse
-from django.core.mail import send_mail
 import re
 import time
+from django import newforms as forms
+from django import template
+from django.conf import settings
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
-from cciw.cciwmain.utils import all, StandardReprMixin
-from cciw.officers.models import Application, Reference
-from cciw.cciwmain.models import Person, Camp
-from cciw.cciwmain import common
+from django.contrib.sites.models import Site
+from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.cache import never_cache
+from cciw.cciwmain import common
+from cciw.cciwmain.models import Person, Camp
+from cciw.cciwmain.utils import all, StandardReprMixin
 from cciw.officers.applications import application_to_text, application_to_rtf, application_rtf_filename, application_txt_filename
 from cciw.officers.email_utils import send_mail_with_attachments, formatted_email
+from cciw.officers.models import Application, Reference
 
 def _copy_application(application):
     new_obj = Application(id=None)
