452288787daf6bc8c28b6fe4fc01153a347a2a8c
Luke Plant <L.Plant.98@cantab.net>
2008-07-05 21:24:06 +0000

    Added webfaction API module

---
+++ cciw/webfaction.py
@@ -0,0 +1,22 @@
+# See http://wf.davidsissitka.com/api/reference/
+
+class WebFactionSession(object):
+    """
+    Class that wraps xmlrpc server calls to always
+    provide the webfaction session id as the first
+    parameter to method calls
+    """
+    def __init__(self, server, session_id):
+        self.server = server
+        self.session_id = session_id
+
+    def __getattr__(self, name):
+        def func(*args):
+            f = getattr(self.server, name)
+            return f(self.session_id, *args)
+        return func
+
+def webfaction_session():
+    server = xmlrpclib.Server('https://api.webfaction.com/')
+    session_id, account = server.login(settings.WEBFACTION_USER, settings.WEBFACTION_PASSWORD)
+    return WebFactionSession(server, session_id)
