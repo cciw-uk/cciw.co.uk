fb8b928d03e9146c66930328e256dbbe02ea9b88
Luke Plant <L.Plant.98@cantab.net>
2008-07-07 12:06:50 +0000

    Added handling of errors for email forwarding

--- scripts/handle_mail.py
+++ scripts/handle_mail.py
@@ -2,9 +2,17 @@
 import _cciw_env
 
 def main():
-    # TODO: error handling
-    from cciw.mail.lists import handle_all_mail
-    handle_all_mail()
-
+    try:
+        from cciw.mail.lists import handle_all_mail
+        handle_all_mail()
+    except:
+        from django.core.mail import mail_admins
+        import traceback
+        import sys
+        subject = 'Sending mail error'
+        exc_info = sys.exc_info()
+        message = '\n'.join(traceback.format_exception(*exc_info))
+        mail_admins(subject, message, fail_silently=True)
+            
 if __name__ == '__main__':
     main()
