fc44f31a0fe7a9cee7cc4c5ad4bec45511db4e38
Luke Plant <L.Plant.98@cantab.net>
2008-07-28 09:45:00 +0000

    Added tests for automatic deletion of Tags

--- cciw/tagging/tests/basic.py
+++ cciw/tagging/tests/basic.py
@@ -13,6 +13,10 @@ class TestTag(TestCase):
         Tag.objects.all().delete()
 
     def test_create(self):
+        """
+        Test basic creation, implicitly testing GenericForeignKey
+        and related utility functions.
+        """
         m = Member.objects.get(user_name='test_member_1')
         p = Post.objects.get(pk=1)
         t = Tag(text='test')
@@ -29,4 +33,34 @@ class TestTag(TestCase):
         self.assertEqual(get_pk_as_str(m), t.creator_id)
         self.assertEqual(get_content_type_id(m), t.creator_ct_id)
 
+    def test_delete_target(self):
+        """
+        Ensure that tags are deleted when their targets are
+        """
+        m = Member.objects.get(user_name='test_member_1')
+        p = Post.objects.get(pk=1)
+        t = Tag(text='test')
+        t.target = p
+        t.creator = m
+        t.save()
 
+        p_id = get_pk_as_str(p) # p.delete will set p.id = None
+        self.assertEqual(Tag.objects.filter(target_id=p_id).count(), 1)
+        p.delete()
+        self.assertEqual(Tag.objects.filter(target_id=p_id).count(), 0)
+
+    def test_delete_creator(self):
+        """
+        Ensure that tags are deleted when their creators are
+        """
+        m = Member.objects.get(user_name='test_member_1')
+        p = Post.objects.get(pk=1)
+        t = Tag(text='test')
+        t.target = p
+        t.creator = m
+        t.save()
+
+        m_id = get_pk_as_str(m) # m.delete will set m.id = None
+        self.assertEqual(Tag.objects.filter(creator_id=m_id).count(), 1)        
+        m.delete()
+        self.assertEqual(Tag.objects.filter(creator_id=m_id).count(), 0)
