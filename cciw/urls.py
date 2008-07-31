354eb8dabdf739ad8180ca0a6edc9027a9b48251
Luke Plant <L.Plant.98@cantab.net>
2008-07-31 21:12:14 +0000

    Removed unneeded password reset code, due to Django [8162]

--- cciw/urls.py
+++ cciw/urls.py
@@ -6,17 +6,11 @@ import cciw.officers.views
 handler404 = 'cciw.cciwmain.views.handler404'
 
 urlpatterns = patterns('',
-    # Override the admin for some views:
-    (r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset',
-     dict(password_reset_form=cciw.officers.views.PasswordResetForm, 
-          template_name='cciw/officers/password_reset_form.html',
-          email_template_name='cciw/officers/password_reset_email.txt')),
-    (r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done',
-     dict(template_name='cciw/officers/password_reset_done.html')),
-    (r'^reset/(?P<uid>\d+)-(?P<hash>\S{32})/$', 'cciw.officers.views.password_reset_confirm'),
-    url(r'^reset/done/$', 'django.views.generic.simple.direct_to_template', 
-        name='auth.password_reset_complete',
-        kwargs={'template':'cciw/officers/password_reset_complete.html'}),
+    # Plug in the password reset views
+    (r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset'),
+    (r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
     # Normal views
     (r'^admin/(.*)', admin.site.root),
     (r'^officers/', include('cciw.officers.urls'))
