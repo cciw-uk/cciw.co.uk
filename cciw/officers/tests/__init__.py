4a40a6a03d132b18a8255312a66f2ce4d97ddf9c
Luke Plant <L.Plant.98@cantab.net>
2008-08-01 14:10:37 +0000

    Fixed broken import

--- cciw/officers/tests/__init__.py
+++ cciw/officers/tests/__init__.py
@@ -1,4 +1,3 @@
 from cciw.officers.tests.applications import *
 from cciw.officers.tests.references import *
 from cciw.officers.tests.officer_list import *
-from cciw.officers.tests.passwordreset import *
