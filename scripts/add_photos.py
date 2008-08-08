2607a89b1ad5edef2601566d43e22946019d27c0
Luke Plant <L.Plant.98@cantab.net>
2008-08-08 16:36:36 +0000

    Added some more error checking to add_photos.py script

--- scripts/add_photos.py
+++ scripts/add_photos.py
@@ -33,6 +33,10 @@ def main():
 
         gallery = get_gallery_for_camp(camp)
 
+        if gallery is None:
+            raise Exception(("A gallery for camp %s, %s does not exist." +
+                            "It is not created until the day the camp ends.") % (camp.number, camp.year))
+
         try:
             p = Photo.objects.get(gallery=gallery, filename=photoname)
         except Photo.DoesNotExist:
