c6fac4fc5cf74e24267732dbe6585e10b8dd31fc
Luke Plant <L.Plant.98@cantab.net>
2010-11-11 23:28:54 +0000

    Updated test settings file.

--- cciw/settings_tests.py
+++ cciw/settings_tests.py
@@ -6,18 +6,17 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-DATABASES['default']['NAME'] = ':memory:'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
 
 MIDDLEWARE_CLASSES = (
-    "django.middleware.common.CommonMiddleware",
-#    "django.middleware.cache.CacheMiddleware",
-#    "django.middleware.gzip.GZipMiddleware",
-#    "lukeplant_me_uk.django.validator.middleware.ValidatorMiddleware",
-#    "django.contrib.csrf.middleware.CsrfViewMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "cciw.middleware.threadlocals.ThreadLocals",
 )
 
