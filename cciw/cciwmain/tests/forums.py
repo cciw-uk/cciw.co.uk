de3292d18d0e94d987132da8b2c8968d4257a2ef
Luke Plant <L.Plant.98@cantab.net>
2010-11-30 02:01:10 +0000

    Small clean-ups to tests

--- cciw/cciwmain/tests/forums.py
+++ cciw/cciwmain/tests/forums.py
@@ -75,20 +75,15 @@ class TopicPage(TestCase):
     def test_topic_html(self):
         response = self.client.get(self.path())
         self.assertEqual(response.status_code, 200)
-        self.assertTrue("<h2>&lt;Jill &amp; Jane&gt;</h2>" in response.content,
-                        "Subject not escaped correctly")
-        self.assertTrue("A <b>unique message</b> with some bbcode &amp; &lt;stuff&gt; to be escaped" in response.content,
-                        "Posts not escaped correctly")
-        self.assertTrue('<a href="/camps/">Forums and photos</a>' in response.content,
-                        "Breadcrumb not escaped properly")
+        self.assertContains(response, "<h2>&lt;Jill &amp; Jane&gt;</h2>")
+        self.assertContains(response, "A <b>unique message</b> with some bbcode &amp; &lt;stuff&gt; to be escaped")
+        self.assertContains(response, '<a href="/camps/">Forums and photos</a>')
 
     def test_topic_atom(self):
         response = self.client.get(self.path(), {'format':'atom'})
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('<title>CCIW - Posts on topic "&lt;Jill &amp; Jane&gt;"</title>' in response.content,
-                        "Title not escaped properly")
-        self.assertTrue('A &lt;b&gt;unique message&lt;/b&gt; with some bbcode &amp;amp; &amp;lt;stuff&amp;gt; to be escaped' in response.content,
-                        "Message posts not escaped properly")
+        self.assertContains(response, '<title>CCIW - Posts on topic "&lt;Jill &amp; Jane&gt;"</title>')
+        self.assertContains(response, 'A &lt;b&gt;unique message&lt;/b&gt; with some bbcode &amp;amp; &amp;lt;stuff&amp;gt; to be escaped')
         self.assertEqual(response['Content-Type'], 'application/atom+xml')
 
     def test_query_count(self):
