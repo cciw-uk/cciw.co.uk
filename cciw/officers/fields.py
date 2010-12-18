d3e8296f2cadd0c4186dd308ea729c3cc08bc114
Luke Plant <L.Plant.98@cantab.net>
2010-12-18 21:23:11 +0000

    Fixed introspection of custom fields for South.

--- cciw/officers/fields.py
+++ cciw/officers/fields.py
@@ -2,6 +2,7 @@ from django.db import models
 import cciw.middleware.threadlocals as threadlocals
 from cciw.officers import formfields
 
+
 class YyyyMmField(models.CharField):
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 7
@@ -13,6 +14,7 @@ class YyyyMmField(models.CharField):
         defaults.update(kwargs)
         return super(YyyyMmField, self).formfield(*args, **defaults)
 
+
 class AddressField(models.TextField):
     def __init__(self, *args, **kwargs):
         kwargs['help_text'] = u'Full address, including post code and country'
@@ -23,10 +25,6 @@ class ExplicitBooleanField(models.NullBooleanField):
         kwargs['default'] = None
         super(ExplicitBooleanField, self).__init__(*args, **kwargs)
 
-if not threadlocals.is_web_request():
-    # When installing, we need the following line.  It is only
-    # executed in the command line context.
-    ExplicitBooleanField = models.NullBooleanField
 
 def required_field(field_class, *args, **kwargs):
     """
@@ -39,11 +37,24 @@ def required_field(field_class, *args, **kwargs):
             f = super(NewDBField, self).formfield(*args, **kwargs)
             f.required_field = True
             return f
-    NewDBField.__name__ = "Required" + field_class.__name__
+
+    if not threadlocals.is_web_request():
+        # Allow South to find it by giving it a unique name and putting it in
+        # this module.
+        import sys
+        NewDBField.__name__ = "Required" + field_class.__name__
+        sys.modules['cciw.officers.fields'].__dict__[NewDBField.__name__] = NewDBField
+
     return NewDBField(*args, **kwargs)
 
-# Allow South to introspect these - they are all based on builtin fields.
-from south.modelsinspector import add_introspection_rules
-add_introspection_rules([], ["^cciw\.officers\.fields\.Required*"])
-add_introspection_rules([], ["^cciw\.officers\.fields\.YyyyMmField"])
-add_introspection_rules([], ["^cciw\.officers\.fields\.AddressField"])
+
+if not threadlocals.is_web_request():
+    # Allow South to introspect these - they are all based on builtin fields.
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^cciw\.officers\.fields\.Required*"])
+    add_introspection_rules([], ["^cciw\.officers\.fields\.YyyyMmField"])
+    add_introspection_rules([], ["^cciw\.officers\.fields\.AddressField"])
+
+    # Could probably have done this using an introspection rule, but for
+    # compatiblity with how we started we must do it this way.
+    ExplicitBooleanField = models.NullBooleanField
