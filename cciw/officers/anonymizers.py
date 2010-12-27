60b76f061c16ecc2ea8969f218cfa63d5da87ff8
Luke Plant <L.Plant.98@cantab.net>
2010-12-27 22:15:27 +0000

    Updated for change in django-anonymizer

--- cciw/officers/anonymizers.py
+++ cciw/officers/anonymizers.py
@@ -10,17 +10,17 @@ class UserAnonymizer(Anonymizer):
 
     model = User
 
-    attributes = {
-        'username':   'username',
-        'first_name': 'first_name',
-        'last_name':  'last_name',
-        'email':      'email',
+    attributes = [
+        ('username',   'username'),
+        ('first_name', 'first_name'),
+        ('last_name',  'last_name'),
+        ('email',      'email'),
         # Set the date_joined to a similar time to when they actually joined,
         # by passing the 'val' paramater
-        'date_joined': lambda self, obj, field, val: self.faker.datetime(field=field, val=val),
+        ('date_joined', 'similar_datetime'),
         # Set to today:
-        'last_login': lambda *args: datetime.now(),
-    }
+        ('last_login', lambda *args: datetime.now()),
+    ]
 
     def alter_object(self, obj):
         if obj.is_superuser:
@@ -36,63 +36,63 @@ class ApplicationAnonymizer(Anonymizer):
 
     model = Application
 
-    attributes = {
+    attributes = [
         # 'full_name': "name", - see below
         #'full_maiden_name': "name",
-        'birth_date': "date",
-        'birth_place': "city",
-        'address_firstline': "street_address",
-        'address_town': "city",
-        'address_county': "uk_county",
-        'address_postcode': "uk_postcode",
-        'address_country': "uk_country",
-        'address_tel': "phonenumber",
-        'address_mobile': "phonenumber",
-        'address_email': "email",
-        'address_since': yyyymmpattern,
-        'address2_from': yyyymmpattern,
-        'address2_to': yyyymmpattern,
-        'address2_address': "full_address",
-        'address3_from': yyyymmpattern,
-        'address3_to': yyyymmpattern,
-        'address3_address': "full_address",
-        'christian_experience': "lorem",
-        'youth_experience': "lorem",
-        'youth_work_declined': "bool",
-        'youth_work_declined_details': "lorem",
-        'relevant_illness': "bool",
-        'illness_details': "lorem",
-        'employer1_name': "company",
-        'employer1_from': yyyymmpattern,
-        'employer1_to': yyyymmpattern,
-        'employer1_job': lambda self, obj, field, val: self.faker.varchar(60, field=field),
-        'employer1_leaving': lambda self, obj, field, val: self.faker.varchar(150, field=field),
-        'employer2_name': "company",
-        'employer2_from': yyyymmpattern,
-        'employer2_to': yyyymmpattern,
-        'employer2_job': lambda self, obj, field, val: self.faker.varchar(60, field=field),
-        'employer2_leaving': lambda self, obj, field, val: self.faker.varchar(150, field=field),
-        'referee1_name': "name",
-        'referee1_address': "full_address",
-        'referee1_tel': "phonenumber",
-        'referee1_mobile': "phonenumber",
-        'referee1_email': "email",
-        'referee2_name': "name",
-        'referee2_address': "full_address",
-        'referee2_tel': "phonenumber",
-        'referee2_mobile': "phonenumber",
-        'referee2_email': "email",
-        'crime_declaration': "bool",
-        'crime_details': "lorem",
-        'court_declaration': "bool",
-        'court_details': "lorem",
-        'concern_declaration': "bool",
-        'concern_details': "lorem",
-        'allegation_declaration': "bool",
-        'crb_check_consent': "bool",
-        # 'finished': "bool", - leave as is
-        'date_submitted': "date",
-    }
+        ('birth_date', "date"),
+        ('birth_place', "city"),
+        ('address_firstline', "street_address"),
+        ('address_town', "city"),
+        ('address_county', "uk_county"),
+        ('address_postcode', "uk_postcode"),
+        ('address_country', "uk_country"),
+        ('address_tel', "phonenumber"),
+        ('address_mobile', "phonenumber"),
+        ('address_email', "email"),
+        ('address_since', yyyymmpattern),
+        ('address2_from', yyyymmpattern),
+        ('address2_to', yyyymmpattern),
+        ('address2_address', "full_address"),
+        ('address3_from', yyyymmpattern),
+        ('address3_to', yyyymmpattern),
+        ('address3_address', "full_address"),
+        ('christian_experience', "similar_lorem"),
+        ('youth_experience', "similar_lorem"),
+        ('youth_work_declined', "bool"),
+        ('youth_work_declined_details', "similar_lorem"),
+        ('relevant_illness', "bool"),
+        ('illness_details', "similar_lorem"),
+        ('employer1_name', "company"),
+        ('employer1_from', yyyymmpattern),
+        ('employer1_to', yyyymmpattern),
+        ('employer1_job', "varchar"),
+        ('employer1_leaving', "varchar"),
+        ('employer2_name', "company"),
+        ('employer2_from', yyyymmpattern),
+        ('employer2_to', yyyymmpattern),
+        ('employer2_job', "varchar"),
+        ('employer2_leaving', "varchar"),
+        ('referee1_name', "name"),
+        ('referee1_address', "full_address"),
+        ('referee1_tel', "phonenumber"),
+        ('referee1_mobile', "phonenumber"),
+        ('referee1_email', "email"),
+        ('referee2_name', "name"),
+        ('referee2_address', "full_address"),
+        ('referee2_tel', "phonenumber"),
+        ('referee2_mobile', "phonenumber"),
+        ('referee2_email', "email"),
+        ('crime_declaration', "bool"),
+        ('crime_details', "lorem"),
+        ('court_declaration', "bool"),
+        ('court_details', "lorem"),
+        ('concern_declaration', "bool"),
+        ('concern_details', "lorem"),
+        ('allegation_declaration', "bool"),
+        ('crb_check_consent', "bool"),
+        # 'finished', "bool", - leave as is
+        ('date_submitted', "date"),
+    ]
 
     order = 2
 
@@ -106,15 +106,15 @@ class ReferenceFormAnonymizer(Anonymizer):
 
     model = ReferenceForm
 
-    attributes = {
-        'referee_name': "name",
-        'how_long_known': lambda self, obj, field, val: self.faker.varchar(150, field=field),
-        'capacity_known': "lorem",
-        'known_offences': "bool",
-        'known_offences_details': "lorem",
-        'capability_children': "lorem",
-        'character': "lorem",
-        'concerns': "lorem",
-        'comments': "lorem",
+    attributes = [
+        ('referee_name', "name"),
+        ('how_long_known', "varchar"),
+        ('capacity_known', "similar_lorem"),
+        ('known_offences', "bool"),
+        ('known_offences_details', "lorem"),
+        ('capability_children', "similar_lorem"),
+        ('character', "similar_lorem"),
+        ('concerns', "lorem"),
+        ('comments', "similar_lorem"),
         # 'date_created': "date",
-    }
+    ]
