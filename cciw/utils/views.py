7dcdf63d1656eb3d94f60d988f8f38e21a6d378c
Luke Plant <L.Plant.98@cantab.net>
2011-02-17 16:01:33 +0000

    Fixed HTML error.

--- cciw/utils/views.py
+++ cciw/utils/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
 def close_window_response():
-    return HttpResponse('<script type="text/javascript">window.close()</script>')
+    return HttpResponse("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+            "http://www.w3.org/TR/html4/loose.dtd"><html><head><title>Close</title><script type="text/javascript">window.close()</script></head><body></body></html>""")
