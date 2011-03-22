5c8ce6a2893c5b54ac245eef6eaa1dc536840608
Luke Plant <L.Plant.98@cantab.net>
2011-03-22 12:30:35 +0000

    Cleanup

--- cciw/cciwmain/admin.py
+++ cciw/cciwmain/admin.py
@@ -27,6 +27,7 @@ class CampAdmin(admin.ModelAdmin):
         return camp.leaders_formatted
     list_display = ('year', 'number', leaders, 'chaplain', 'age', 'site', 'start_date')
     list_display_links = ('number', leaders)
+    del leaders
     list_filter = ('age', 'site')
     filter_horizontal = ('leaders', 'admins')
     date_hierarchy = 'start_date'
