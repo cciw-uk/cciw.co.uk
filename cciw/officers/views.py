b224ccc8d86c8d98f2e0b9f1d2ab4509f722d964
Luke Plant <L.Plant.98@cantab.net>
2011-05-03 02:16:32 +0100

    Fixed permissions to allow camp secretaries access to leaders pages

--- cciw/officers/views.py
+++ cciw/officers/views.py
@@ -54,19 +54,23 @@ def _copy_application(application):
     return new_obj
 
 
+SECRETARY_GROUP_NAME = 'Secretaries'
+LEADER_GROUP_NAME = 'Leaders'
+
 def _is_camp_admin(user):
     """
-    Returns True if the user is an admin for any camp.
+    Returns True if the user is an admin for any camp, or has rights
+    for editing camp/officer/reference/CRB information
     """
-    return (user.groups.filter(name='Leaders').exists()) \
+    return (user.groups.filter(name=LEADER_GROUP_NAME) |
+            user.groups.filter(name=SECRETARY_GROUP_NAME)).exists() \
         or user.camps_as_admin.exists() > 0
-
 
 camp_admin_required = user_passes_test(_is_camp_admin)
 
 
 def _is_cciw_secretary(user):
-    return user.groups.filter(name='Secretaries').exists()
+    return user.groups.filter(name=SECRETARY_GROUP_NAME).exists()
 
 
 def _is_camp_officer(user):
