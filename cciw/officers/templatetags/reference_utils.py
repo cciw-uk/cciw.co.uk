16992a94851dab8ec3a675b79f35991f0ea075e5
Luke Plant <L.Plant.98@cantab.net>
2011-05-03 01:28:50 +0100

    Removed some old dead code

--- cciw/officers/templatetags/reference_utils.py
+++ cciw/officers/templatetags/reference_utils.py
@@ -5,12 +5,6 @@ from django.utils.html import escape
 
 register = template.Library()
 
-@register.filter(name='firstline')
-@stringfilter
-def firstline(value):
-    # Assumes autoescape is on.
-    parts = map(escape, value.strip().split("\n"))
-    return mark_safe("<div class='firstline'>%s</div><div class='hidden addrblock'>%s</div>" % (parts[0], '<br />'.join(parts[1:])))
 
 @register.filter(name='indent')
 @stringfilter
