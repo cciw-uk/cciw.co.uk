c3f88e630c466cf31cbcdb60d9bb015c213de807
Luke Plant <L.Plant.98@cantab.net>
2011-05-10 00:48:32 +0100

    Updated AJAX CSRF code to match Django docs

--- cciw/cciwmain/static/js/cciwutils.js
+++ cciw/cciwmain/static/js/cciwutils.js
@@ -125,7 +125,11 @@
                 // or any other URL that isn't scheme relative or absolute i.e relative.
                 !(/^(\/\/|http:|https:).*/.test(url));
         }
-        if (sameOrigin(settings.url)) {
+        function safeMethod(method) {
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+
+        if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
         }
     });
