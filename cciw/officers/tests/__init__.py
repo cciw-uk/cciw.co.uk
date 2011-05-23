4e8445c607e55817efc9cc4f9b6f07ba537a284f
Luke Plant <L.Plant.98@cantab.net>
2011-05-23 13:35:20 +0100

    Fixed old import

--- cciw/officers/tests/__init__.py
+++ cciw/officers/tests/__init__.py
@@ -1,5 +1,4 @@
 from cciw.officers.tests.applications import *
-from cciw.officers.tests.crbs import *
 from cciw.officers.tests.references import *
 from cciw.officers.tests.applicationform import *
 from cciw.officers.tests.utils import *
