1b7f5037dd39d69d2c658e62d44dd7a665240b9e
Luke Plant <L.Plant.98@cantab.net>
2011-06-13 18:30:14 +0100

    Fixed manage.py to use correct settings so we don't need DJANGO_SETTINGS_MODULE
    (since we've already customised the way manage.py works we may as well)

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.5
 import warnings
 #warnings.simplefilter("error", PendingDeprecationWarning)
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cciw.settings'
 from django.core import management
 if __name__ == "__main__":
     management.execute_from_command_line()
