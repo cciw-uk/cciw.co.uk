f03373b1453f8f6c90bd3ef664601115ece9b332
Luke Plant <L.Plant.98@cantab.net>
2011-09-29 13:36:17 +0100

    Made use of .basket() manager method instead of manual filtering

--- cciw/bookings/models.py
+++ cciw/bookings/models.py
@@ -373,14 +373,14 @@ class Booking(models.Model):
 
         # 2nd/3rd child discounts
         if self.price_type == PRICE_2ND_CHILD:
-            qs = self.account.bookings.filter(shelved=False, camp__year__exact=self.camp.year)
+            qs = self.account.bookings.basket(self.camp.year)
             if not qs.filter(price_type=PRICE_FULL).exists():
                 errors.append(u"You cannot use a 2nd child discount unless you have "
                               u"a child at full price. Please edit the place details "
                               u"and choose an appropriate price type.")
 
         if self.price_type == PRICE_3RD_CHILD:
-            qs = self.account.bookings.filter(shelved=False, camp__year__exact=self.camp.year)
+            qs = self.account.bookings.basket(self.camp.year)
             qs = qs.filter(price_type=PRICE_FULL) | qs.filter(price_type=PRICE_2ND_CHILD)
             if qs.count() < 2:
                 errors.append(u"You cannot use a 3rd child discount unless you have "
@@ -467,9 +467,8 @@ class Booking(models.Model):
 
 
         if self.price_type == PRICE_FULL:
-            full_pricers = self.account.bookings.filter(price_type=PRICE_FULL,
-                                                        camp__year__exact=self.camp.year,
-                                                        shelved=False).order_by('name')
+            full_pricers = self.account.bookings.basket(self.camp.year)\
+                .filter(price_type=PRICE_FULL).order_by('name')
             if len(full_pricers) > 1:
                 names = [b.name for b in full_pricers]
                 pretty_names = u', '.join(names[1:]) + u" and " + names[0]
@@ -484,9 +483,8 @@ class Booking(models.Model):
                 warnings.append(warning)
 
         if self.price_type == PRICE_2ND_CHILD:
-            second_childers = self.account.bookings.filter(price_type=PRICE_2ND_CHILD,
-                                                           camp__year__exact=self.camp.year,
-                                                           shelved=False).order_by('name')
+            second_childers = self.account.bookings.basket(self.camp.year)\
+                .filter(price_type=PRICE_2ND_CHILD).order_by('name')
             if len(second_childers) > 1:
                 names = [b.name for b in second_childers]
                 pretty_names = u', '.join(names[1:]) + u" and " + names[0]
