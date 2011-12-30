986db95bc99237b046168f0f145b9ba9a12b9f7a
Luke Plant <L.Plant.98@cantab.net>
2011-12-30 17:41:06 +0000

    Added facility to send message to booking secretary, with help added to various booking pages

--- cciw/cciwmain/tests/contact.py
+++ cciw/cciwmain/tests/contact.py
@@ -1,6 +1,7 @@
 from BeautifulSoup import BeautifulSoup
 from cciw.cciwmain.tests.client import CciwClient
 from django.test import TestCase
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.core import mail
 
@@ -15,19 +16,22 @@ class ContactUsPage(TestCase):
     def test_cant_send_without_email(self):
         self.client.post(FEEDBACK_URL, data = dict(name="My Name",
                                                    email="",
-                                                   message="The Message"))
+                                                   message="The Message",
+                                                   subject="website"))
         self.assertEqual(len(mail.outbox), 0)
 
     def test_cant_send_without_valid_email(self):
         self.client.post(FEEDBACK_URL, data = dict(name="My Name",
                                                    email="invalidemail",
-                                                   message="The Message"))
+                                                   message="The Message",
+                                                   subject="website"))
         self.assertEqual(len(mail.outbox), 0)
 
     def test_cant_send_without_message(self):
         self.client.post(FEEDBACK_URL, data = dict(name="My Name",
                                                    email="validemail@somewhere.com",
-                                                   message=""))
+                                                   message="",
+                                                   subject="website"))
         self.assertEqual(len(mail.outbox), 0)
 
     def test_form_appears(self):
@@ -40,5 +44,18 @@ class ContactUsPage(TestCase):
     def test_send(self):
         self.client.post(FEEDBACK_URL, data = dict(name="My Name",
                                                    email="validemail@somewhere.com",
-                                                   message="The Message"))
+                                                   message="The Message",
+                                                   subject="general"))
         self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [settings.FEEDBACK_EMAIL])
+
+    def test_send_to_booking_secretary(self):
+        self.client.post(FEEDBACK_URL,
+                         data = dict(name="My Name",
+                                     email="validemail@somewhere.com",
+                                     message="The Message",
+                                     subject="bookings"))
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(sorted(mail.outbox[0].to),
+                         sorted([settings.BOOKING_SECRETARY_EMAIL,
+                                 settings.FEEDBACK_EMAIL]))
