5dac1308234a6f5ea55d1eecfc17a554b7006000
Luke Plant <L.Plant.98@cantab.net>
2013-11-19 17:45:15 +0000

    Use Django's login form, now that it has the password reset link

--- cciw/urls.py
+++ cciw/urls.py
@@ -33,10 +33,10 @@ autocomplete.register(
 urlpatterns = patterns('',
     (r'^booking/', include('cciw.bookings.urls')),
     # Plug in the password reset views
-    (r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset'),
-    (r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name="admin_password_reset"),
+    url(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name="password_reset_done"),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name="password_reset_confirm"),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name="password_reset_complete"),
     # Normal views
     (r'^admin/', include(admin.site.urls)),
     (r'^officers/', include('cciw.officers.urls')),
