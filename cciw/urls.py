511b8a74cf7cabf3ca28cc1553301f7a207ae2d4
Luke Plant <L.Plant.98@cantab.net>
2014-01-28 18:47:51 +0000

    Corrected password reset URL

--- cciw/urls.py
+++ cciw/urls.py
@@ -53,7 +53,7 @@ urlpatterns = patterns('',
     # Plug in the password reset views
     url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name="admin_password_reset"),
     url(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name="password_reset_done"),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name="password_reset_confirm"),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name="password_reset_confirm"),
     url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name="password_reset_complete"),
     # Normal views
     (r'^admin/', include(admin.site.urls)),
