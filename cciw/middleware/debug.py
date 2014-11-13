f063a59532d3dccf38e938e93f5a25acf6926fd7
Luke Plant <L.Plant.98@cantab.net>
2014-11-13 18:40:00 +0000

    Better debug middleware for pretending to be other users

--- cciw/middleware/debug.py
+++ cciw/middleware/debug.py
@@ -7,3 +7,16 @@ class DebugMiddleware(object):
             set_booking_account_cookie(response, BookingAccount.objects.get(email=request.GET['booking_account']))
 
         return response
+
+    def process_request(self, request):
+
+        if 'as' in request.GET:
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+            user = User.objects.get(username=request.GET['as'])
+            request.session['_auth_user_id'] = user.id
+            request.session['_auth_user_backend'] = 'django.contrib.auth.backends.ModelBackend'
+            request.user = user
+            request.session.save()
+
