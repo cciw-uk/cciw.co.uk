6065066b58ed50bccb276a0309405c028fdb7078
Luke Plant <L.Plant.98@cantab.net>
2015-03-13 12:08:46 +0000

    Fixed error in tests due to missing HtmlChunk

--- cciw/cciwmain/templatetags/standardpage.py
+++ cciw/cciwmain/templatetags/standardpage.py
@@ -19,24 +19,40 @@ def do_email(parser, token):
     parser.delete_first_token()
     return EmailNode(nodelist)
 
+
 class RenderHtmlChunk(template.Node):
-    def __init__(self, chunk_name):
+    def __init__(self, chunk_name, ignore_missing=False):
         self.chunk_name = chunk_name
+        self.ignore_missing = ignore_missing
 
     def render(self, context):
         chunk = getattr(self, 'chunk', None)
         if chunk is None:
-            chunk = HtmlChunk.objects.get(name=self.chunk_name)
+            try:
+                chunk = HtmlChunk.objects.get(name=self.chunk_name)
+            except HtmlChunk.DoesNotExist:
+                if not self.ignore_missing:
+                    raise
+                chunk = None
             self.chunk = chunk
+        if chunk is None:
+            return ''
         return chunk.render(context['request'])
+
 
 def do_htmlchunk(parser, token):
     """
     Renders an HtmlChunk. It takes a single argument,
     the name of the HtmlChunk to find.
+
+    If 'ignoremissing' is passed as second argument, errors will be ignored.
     """
-    bits = token.contents.split(" ", 1)
-    return RenderHtmlChunk(bits[1])
+    bits = token.contents.split(" ")
+    if len(bits) == 2:
+        ignore_missing = False
+    else:
+        ignore_missing = bits[2] == 'ignoremissing'
+    return RenderHtmlChunk(bits[1], ignore_missing=ignore_missing)
 
 class AtomFeedLink(template.Node):
     def __init__(self, parser, token):
