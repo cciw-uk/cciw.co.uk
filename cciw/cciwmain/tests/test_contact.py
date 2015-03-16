b5eda320384790036908495cfa0536402a130a50
Luke Plant <L.Plant.98@cantab.net>
2015-03-16 15:51:47 +0000

    Reformatting

--- cciw/cciwmain/tests/test_contact.py
+++ cciw/cciwmain/tests/test_contact.py
@@ -7,6 +7,7 @@ from django.core import mail
 
 CONTACT_US_URL = reverse("cciwmain.misc.contact_us")
 
+
 class ContactUsPage(TestCase):
     fixtures = ['basic.json', 'contact.json']
 
@@ -14,48 +15,48 @@ class ContactUsPage(TestCase):
         self.client = CciwClient()
 
     def test_cant_send_without_email(self):
-        self.client.post(CONTACT_US_URL, data = dict(name="My Name",
-                                                     email="",
-                                                     message="The Message",
-                                                     subject="website"))
+        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
+                                                   email="",
+                                                   message="The Message",
+                                                   subject="website"))
         self.assertEqual(len(mail.outbox), 0)
 
     def test_cant_send_without_valid_email(self):
-        self.client.post(CONTACT_US_URL, data = dict(name="My Name",
-                                                     email="invalidemail",
-                                                     message="The Message",
-                                                     subject="website"))
+        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
+                                                   email="invalidemail",
+                                                   message="The Message",
+                                                   subject="website"))
         self.assertEqual(len(mail.outbox), 0)
 
     def test_cant_send_without_message(self):
-        self.client.post(CONTACT_US_URL, data = dict(name="My Name",
-                                                     email="validemail@somewhere.com",
-                                                     message="",
-                                                     subject="website"))
+        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
+                                                   email="validemail@somewhere.com",
+                                                   message="",
+                                                   subject="website"))
         self.assertEqual(len(mail.outbox), 0)
 
     def test_form_appears(self):
         r = self.client.get(CONTACT_US_URL)
         b = BeautifulSoup(r.content)
-        self.assertNotEqual(b.find(name='input', attrs={'name':'email'}), None)
-        self.assertNotEqual(b.find(name='input', attrs={'name':'name'}), None)
-        self.assertNotEqual(b.find(name='textarea', attrs={'name':'message'}), None)
+        self.assertNotEqual(b.find(name='input', attrs={'name': 'email'}), None)
+        self.assertNotEqual(b.find(name='input', attrs={'name': 'name'}), None)
+        self.assertNotEqual(b.find(name='textarea', attrs={'name': 'message'}), None)
 
     def test_send(self):
-        self.client.post(CONTACT_US_URL, data = dict(name="My Name",
-                                                     email="validemail@somewhere.com",
-                                                     message="The Message",
-                                                     subject="general"))
+        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
+                                                   email="validemail@somewhere.com",
+                                                   message="The Message",
+                                                   subject="general"))
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to, [settings.CONTACT_US_EMAIL])
         self.assertEqual(mail.outbox[0].extra_headers['Reply-To'], 'validemail@somewhere.com')
 
     def test_send_to_booking_secretary(self):
         self.client.post(CONTACT_US_URL,
-                         data = dict(name="My Name",
-                                     email="validemail@somewhere.com",
-                                     message="The Message",
-                                     subject="bookings"))
+                         data=dict(name="My Name",
+                                   email="validemail@somewhere.com",
+                                   message="The Message",
+                                   subject="bookings"))
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(sorted(mail.outbox[0].to),
                          sorted([settings.BOOKING_SECRETARY_EMAIL,
