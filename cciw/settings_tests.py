0c9fa3638348fdc3b29270ae42faab21c0fcac45
Luke Plant <L.Plant.98@cantab.net>
2015-08-20 13:09:32 +0100

    Disable migrations in tests, because they don't work properly

--- cciw/settings_tests.py
+++ cciw/settings_tests.py
@@ -31,3 +31,13 @@ INSTALLED_APPS = list(filter(lambda x: x not in [
 SEND_BROKEN_LINK_EMAILS = False
 
 TEST_DIR = basedir + r'/cciw/cciwmain/tests'
+
+class DisableMigrations(object):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+MIGRATION_MODULES = DisableMigrations()
