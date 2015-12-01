1055ee63311b33fa28396ee145058143ed9638f4
Luke Plant <L.Plant.98@cantab.net>
2015-12-01 14:27:12 +0000

    Nicer admin for selecting users

--- cciw/accounts/models.py
+++ cciw/accounts/models.py
@@ -2,4 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+
+    def __str__(self):
+        return "{0} {1} <{2}>".format(self.first_name, self.last_name, self.email)
