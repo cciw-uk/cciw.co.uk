ffd7ead726a1a337020c24732dc58c5c73817a60
Luke Plant <L.Plant.98@cantab.net>
2015-12-01 13:02:22 +0000

    Data migration to populate correct names

---
+++ cciw/cciwmain/migrations/0017_auto_20151201_1238.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+MAP = {
+    'A': 'Green',
+    'B': 'Blue',
+    'C': 'Orange',
+    'D': 'Purple',
+    'E': 'Red',
+    'F': 'Silver',
+}
+
+
+def forwards(apps, schema_editor):
+    CampName = apps.get_model('cciwmain.CampName')
+    for from_name, to_name in MAP.items():
+        CampName.objects.filter(name=from_name).update(name=to_name, slug=to_name.lower())
+
+
+def backwards(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cciwmain', '0016_remove_camp_previous_camp'),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards, backwards)
+    ]
