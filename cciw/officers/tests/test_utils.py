8a0c909b31678c2a5943048dab38d64dc91e4bc8
Luke Plant <L.Plant.98@cantab.net>
2015-12-02 16:26:27 +0000

    Fixed some failing tests

--- cciw/officers/tests/test_utils.py
+++ cciw/officers/tests/test_utils.py
@@ -8,9 +8,11 @@ class UtilsTests(ExtraOfficersSetupMixin, TestCase):
 
     def test_camp_officer_list(self):
         c = self.default_camp_1
-        self.assertEqual(repr(camp_officer_list(c)), "[<User: fredjones>, <User: joebloggs>, <User: petersmith>]")
+        self.assertEqual([u.username for u in camp_officer_list(c)],
+                         ["fredjones", "joebloggs", "petersmith"])
 
     def test_camp_slacker_list(self):
         c = self.default_camp_1
         self.officer1.applications.create(finished=True, date_submitted="2000-01-01")
-        self.assertEqual(repr(camp_slacker_list(c)), "[<User: fredjones>, <User: petersmith>]")
+        self.assertEqual([u.username for u in camp_slacker_list(c)],
+                         ["fredjones", "petersmith"])
