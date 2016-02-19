cf4d91eb44c8995b71f8c2c2b44843e21019c18d
Luke Plant <L.Plant.98@cantab.net>
2016-02-19 12:11:11 +0000

    Removed temporary fabfile change

--- fabfile.py
+++ fabfile.py
@@ -318,7 +318,6 @@ def fake_migrations_mode():
 def update_database():
     with virtualenv(target.VENV_DIR):
         with cd(target.SRC_DIR):
-            run_venv("./manage.py migrate thumbnail --fake-initial")
             if getattr(env, 'fake_migrations', False):
                 run_venv("./manage.py migrate --fake --noinput")
             else:
