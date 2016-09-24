cda29ffea20ccd207b2e5523566b4dfba4d251af
Luke Plant <L.Plant.98@cantab.net>
2016-09-24 22:22:26 +0300

    Use Mailgun for outgoing mail

--- cciw/settings.py
+++ cciw/settings.py
@@ -107,9 +107,9 @@ INSTALLED_APPS = [
     'wiki.plugins.macros',
     'django_nyt',
     'compressor',
-    'mailer',
     'django_countries',
     'opbeat.contrib.django',
+    'anymail',
 ]
 
 if not (LIVEBOX and WEBSERVER_RUNNING):
@@ -195,21 +195,18 @@ REFERENCES_EMAIL = "CCIW references <references@cciw.co.uk>"
 
 if LIVEBOX:
     if PRODUCTION:
-        EMAIL_BACKEND = "mailer.backend.DbBackend"
+        EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
     elif STAGING:
-        EMAIL_BACKEND = "cciw.mail.backend.StagingBackend"
-
+        EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 if DEVBOX:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_HOST = 'localhost'
-    EMAIL_HOST_USER = None
-    EMAIL_HOST_PASSWORD = None
-    EMAIL_PORT = 8025
 
-else:
-    EMAIL_HOST = "smtp.webfaction.com"
-    from cciw.settings_priv import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+from cciw.settings_priv import MAILGUN_API_KEY
+ANYMAIL = {
+    "MAILGUN_API_KEY": MAILGUN_API_KEY,
+}
+
 
 # == MAILING LISTS ==
 
