00c1e6fed877947a46731097d9d66c2ad2c575ec
Luke Plant <L.Plant.98@cantab.net>
2016-09-29 13:39:04 +0300

    Merged mail tests from officers module into mail module

--- cciw/mail/tests.py
+++ cciw/mail/tests.py
@@ -15,12 +15,14 @@ from cciw.officers.tests.base import ExtraOfficersSetupMixin
 from cciw.utils.tests.base import TestBase
 
 from . import views
-from .lists import MailAccessDenied, NoSuchList, handle_mail, find_list
+from .lists import MailAccessDenied, NoSuchList, extract_email_addresses, find_list, handle_mail
 from .mailgun import send_mime_message
 from .test_data import (MAILGUN_EXAMPLE_POST_DATA_FOR_BOUNCE_ENDPOINT,
                         MAILGUN_EXAMPLE_POST_DATA_FOR_BOUNCE_ENDPOINT_CONTENT_TYPE,
                         MAILGUN_EXAMPLE_POST_DATA_FOR_BOUNCE_ENDPOINT_FOR_REFERENCE,
                         MAILGUN_EXAMPLE_POST_DATA_FOR_MIME_ENDPOINT)
+
+User = get_user_model()
 
 
 def b(s):
@@ -62,7 +64,6 @@ class TestMailingLists(ExtraOfficersSetupMixin, TestBase):
 
     def setUp(self):
         super(TestMailingLists, self).setUp()
-        User = get_user_model()
         User.objects.filter(is_superuser=True).update(is_superuser=False)
         User.objects.create(username="admin1",
                             email="admin1@admin.com",
@@ -71,27 +72,75 @@ class TestMailingLists(ExtraOfficersSetupMixin, TestBase):
                             email="admin2@admin.com",
                             is_superuser=True)
 
-    def test_handle_list(self):
-        with mock_mailgun_send_mime() as m_s:
-            handle_mail(MSG1)
-            self.assertEqual(m_s.call_count, 2)
-            messages_sent = m_s.messages_sent()
-            to_addresses = m_s.to_addresses()
-            self.assertEqual(list(sorted(to_addresses)),
-                             ["admin1@admin.com",
-                              "admin2@admin.com"])
-            self.assertTrue(all(b"Sender: CCIW lists <lists@cciw.co.uk>" in m
-                                for m in messages_sent))
-            self.assertTrue(all(b"From: Joe <joe@gmail.com>" in m
-                                for m in messages_sent))
-            self.assertTrue(any(b"To: admin1@admin.com" in m
-                                for m in messages_sent))
-            self.assertTrue(any(b"To: admin2@admin.com" in m
-                                for m in messages_sent))
-            self.assertTrue(all(b"Subject: Test" in m
-                                for m in messages_sent))
+    def test_invalid_list(self):
+        self.assertRaises(NoSuchList,
+                          lambda: find_list('everyone@cciw.co.uk', 'joe@random.com'))
+        self.assertRaises(NoSuchList,
+                          lambda: find_list('x-camp-2000-blue-officers@cciw.co.uk', 'joe@random.com'))
 
-    def test_handle_list_committee(self):
+    def test_officer_list(self):
+        self.assertRaises(MailAccessDenied,
+                          lambda: find_list('camp-2000-blue-officers@cciw.co.uk',
+                                            'joe@random.com'))
+
+        self.assertRaises(MailAccessDenied,
+                          lambda: find_list('camp-2000-blue-officers@cciw.co.uk',
+                                            self.officer1.email))
+
+        l = find_list('camp-2000-blue-officers@cciw.co.uk', 'LEADER@SOMEWHERE.COM')
+
+        self.assertEqual([u.username for u in l.members],
+                         ["fredjones", "joebloggs", "petersmith"])
+
+    def test_debug_list(self):
+        self.assertEqual(sorted([u.email for u in
+                                 find_list('camp-debug@cciw.co.uk', 'anyone@gmail.com').members]),
+                         ['admin1@admin.com', 'admin2@admin.com'])
+
+    def test_leader_list(self):
+        leader_user = self.leader_user
+
+        # Permissions
+
+        # Officer/non-privileged
+        self.assertRaises(MailAccessDenied,
+                          lambda: find_list('camps-2000-leaders@cciw.co.uk',
+                                            self.officer1.email))
+
+        # superuser:
+        l1 = find_list('camp-2000-blue-leaders@cciw.co.uk', 'ADMIN1@ADMIN.COM')
+
+        # leader:
+        l2 = find_list('camp-2000-blue-leaders@cciw.co.uk', 'LEADER@SOMEWHERE.COM')
+
+        # Contents
+        self.assertEqual(set(find_list('camps-2000-leaders@cciw.co.uk',
+                                       leader_user.email).members),
+                         {self.leader_user})
+
+        self.assertEqual(l1, l2)
+
+    def test_handle_debug_list(self):
+        with mock_mailgun_send_mime() as m_s:
+            handle_mail(MSG_DEBUG_LIST)
+        self.assertEqual(m_s.call_count, 2)
+        messages_sent = m_s.messages_sent()
+        to_addresses = m_s.to_addresses()
+        self.assertEqual(list(sorted(to_addresses)),
+                         ["admin1@admin.com",
+                          "admin2@admin.com"])
+        self.assertTrue(all(b"Sender: CCIW lists <lists@cciw.co.uk>" in m
+                            for m in messages_sent))
+        self.assertTrue(all(b"From: Joe <joe@gmail.com>" in m
+                            for m in messages_sent))
+        self.assertTrue(any(b"To: admin1@admin.com" in m
+                            for m in messages_sent))
+        self.assertTrue(any(b"To: admin2@admin.com" in m
+                            for m in messages_sent))
+        self.assertTrue(all(b"Subject: Test" in m
+                            for m in messages_sent))
+
+    def test_handle_committee_list(self):
         committee, _ = Group.objects.get_or_create(name=COMMITTEE_GROUP_NAME)
         committee.user_set.create(
             username="aman1",
@@ -100,25 +149,48 @@ class TestMailingLists(ExtraOfficersSetupMixin, TestBase):
             username="awoman1",
             email="a.woman@example.com")
 
-        msg = MSG1.replace('camp-debug@cciw.co.uk', 'committee@cciw.co.uk')
+        msg = MSG_COMMITTEE_LIST.replace('a.woman@example.com', 'joe@gmail.com')
         with mock_mailgun_send_mime() as m_s:
             handle_mail(msg)
 
         self.assertEqual(m_s.call_count, 0)  # Permission denied
-        msg2 = msg.replace('joe@gmail.com', 'a.woman@example.com')
 
         with mock_mailgun_send_mime() as m_s2:
-            handle_mail(msg2)
+            handle_mail(MSG_COMMITTEE_LIST)
 
-            messages_sent = m_s2.messages_sent()
-            to_addresses = m_s2.to_addresses()
-            self.assertEqual(list(sorted(to_addresses)),
-                             ["a.man@example.com",
-                              "a.woman@example.com"])
-            self.assertTrue(all(b"Sender: committee@cciw.co.uk" in m
-                                for m in messages_sent))
-            self.assertTrue(all(b"List-Post: <mailto:committee@cciw.co.uk>" in m
-                                for m in messages_sent))
+        messages_sent = m_s2.messages_sent()
+        to_addresses = m_s2.to_addresses()
+        self.assertEqual(list(sorted(to_addresses)),
+                         ["a.man@example.com",
+                          "a.woman@example.com"])
+        self.assertTrue(all(b"Sender: committee@cciw.co.uk" in m
+                            for m in messages_sent))
+        self.assertTrue(all(b"List-Post: <mailto:committee@cciw.co.uk>" in m
+                            for m in messages_sent))
+
+    def test_handle_officer_list(self):
+        with mock_mailgun_send_mime() as m_s:
+            handle_mail(MSG_OFFICER_LIST)
+
+        sent_messages = m_s.messages_sent()
+        self.assertEqual(len(sent_messages), 3)
+
+        self.assertTrue(all(b'From: Dave Stott <leader@somewhere.com>' in m for m in sent_messages))
+        self.assertEqual(m_s.call_args_list[0][0][0], '"Fred Jones" <fredjones@somewhere.com>')
+        self.assertIn(b"Sender: CCIW lists", sent_messages[0])
+        self.assertIn(b"From: Dave Stott <leader@somewhere.com>", sent_messages[0])
+
+    def test_handle_officer_list_bounce(self):
+        bad_mail = MSG_OFFICER_LIST.replace("leader@somewhere.com", "notleader@somewhere.com")
+        with mock_mailgun_send_mime() as m_s:
+            handle_mail(bad_mail)
+        self.assertEqual(m_s.messages_sent(), [])
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "Access to mailing list camp-2000-blue-officers@cciw.co.uk denied")
+
+    def test_extract(self):
+        self.assertEqual(extract_email_addresses('Some Guy <A.Body@example.com>'),
+                         ['A.Body@example.com'])
 
     def test_handle_mail_exception(self):
         """
@@ -130,12 +202,13 @@ class TestMailingLists(ExtraOfficersSetupMixin, TestBase):
             def connection_error():
                 raise ConnectionError("Connection refused")
             m_s.side_effect = connection_error
-            self.assertRaises(Exception, handle_mail, MSG1)
+            self.assertRaises(Exception, handle_mail, MSG_DEBUG_LIST)
 
     def test_handle_invalid_list(self):
         with mock_mailgun_send_mime() as m_s:
             with mock_send_mail() as send_mail:
-                msg = MSG1.replace('camp-debug@cciw.co.uk', 'camp-1990-blue-officers@cciw.co.uk')
+                msg = MSG_DEBUG_LIST.replace('camp-debug@cciw.co.uk',
+                                             'camp-1990-blue-officers@cciw.co.uk')
                 handle_mail(msg)
         self.assertEqual(m_s.call_count, 0)
         self.assertEqual(send_mail.call_count, 1)
@@ -157,67 +230,30 @@ class TestMailingLists(ExtraOfficersSetupMixin, TestBase):
 
         with mock_mailgun_send_mime() as m_s:
             with mock_send_mail() as send_mail:
-                handle_mail(MSG1)
-                # We should have tried to send to all recipients
-                self.assertEqual(m_s.call_count, 3)
+                handle_mail(MSG_DEBUG_LIST)
+        # We should have tried to send to all recipients
+        self.assertEqual(m_s.call_count, 3)
 
-                # Should have reported the error
-                self.assertEqual(send_mail.call_count, 1)
-                error_email = send_mail.sent_messages(0)
-                self.assertIn("admin1@faildomain.com",
-                              error_email.body)
-                self.assertEqual(error_email.subject,
-                                 "Error with email to list camp-debug@cciw.co.uk")
-                self.assertEqual(error_email.to,
-                                 ["Joe <joe@gmail.com>"])
-
-    def test_find_list(self):
-        leader_user = self.leader_user
-
-        # non-existent
-        self.assertRaises(NoSuchList,
-                          lambda: find_list('no-such-list@cciw.co.uk',
-                                            leader_user.email))
-
-        # camp-debug
-        self.assertEqual(sorted([u.email for u in
-                                 find_list('camp-debug@cciw.co.uk', 'anyone@gmail.com').members]),
-                         ['admin1@admin.com', 'admin2@admin.com'])
-
-        # officers
-        self.assertRaises(MailAccessDenied,
-                          lambda: find_list('camp-2000-blue-officers@cciw.co.uk',
-                                            'anyone@gmail.com'))
-
-        self.assertRaises(MailAccessDenied,
-                          lambda: find_list('camp-2000-blue-officers@cciw.co.uk',
-                                            self.officer1.email))
-
-        self.assertEqual(set(find_list('camp-2000-blue-officers@cciw.co.uk',
-                                       leader_user.email).members),
-                         {self.officer1,
-                          self.officer2,
-                          self.officer3})
-
-        # leaders
-        self.assertRaises(MailAccessDenied,
-                          lambda: find_list('camps-2000-leaders@cciw.co.uk',
-                                            self.officer1.email))
-
-        self.assertEqual(set(find_list('camps-2000-leaders@cciw.co.uk',
-                                       leader_user.email).members),
-                         {self.leader_user})
+        # Should have reported the error
+        self.assertEqual(send_mail.call_count, 1)
+        error_email = send_mail.sent_messages(0)
+        self.assertIn("admin1@faildomain.com",
+                      error_email.body)
+        self.assertEqual(error_email.subject,
+                         "Error with email to list camp-debug@cciw.co.uk")
+        self.assertEqual(error_email.to,
+                         ["Joe <joe@gmail.com>"])
 
     def test_handle_mail_permission_denied(self):
-        MSG = MSG1.replace('camp-debug@cciw.co.uk',
-                           'camp-2000-blue-officers@cciw.co.uk')
+        MSG = MSG_OFFICER_LIST.replace('leader@somewhere.com',
+                                       'someone@somewhere.com')
         with mock_mailgun_send_mime() as m_s:
             with mock_send_mail() as send_mail:
                 handle_mail(MSG)
-                sent = m_s.messages_sent()
-                self.assertEqual(len(sent), 0)
-                self.assertEqual(send_mail.call_count, 1)
-                self.assertIn("you do not have permission", send_mail.sent_messages(0).body)
+            sent = m_s.messages_sent()
+            self.assertEqual(len(sent), 0)
+            self.assertEqual(send_mail.call_count, 1)
+            self.assertIn("you do not have permission", send_mail.sent_messages(0).body)
 
     def test_mailgun_incoming(self):
         rf = RequestFactory()
@@ -295,7 +331,7 @@ def emailify(msg):
     return msg.strip().replace("\n", "\r\n")
 
 
-MSG1 = emailify("""
+MSG_DEBUG_LIST = emailify("""
 MIME-Version: 1.0
 Content-Type: text/plain; charset="utf-8"
 Content-Transfer-Encoding: 7bit
@@ -306,6 +342,24 @@ Date: Sun, 28 Feb 2016 22:32:03 -0000
 Message-ID: <56CCDE2E.9030103@gmail.com>
 
 Test message
+""")
+
+MSG_COMMITTEE_LIST = (MSG_DEBUG_LIST
+                      .replace('camp-debug@cciw.co.uk', 'committee@cciw.co.uk')
+                      .replace('joe@gmail.com', 'a.woman@example.com')
+                      )
+
+MSG_OFFICER_LIST = emailify("""
+MIME-Version: 1.0
+Date: Thu, 30 Jul 2015 10:39:10 +0100
+To: "Camp 1 officers" <camp-2000-blue-officers@cciw.co.uk>
+Subject: Minibus Drivers
+Content-Type: text/plain; charset="us-ascii"
+From: Dave Stott <leader@somewhere.com>
+Content-Type: text/plain; charset=utf-8
+
+This is a message!
+
 """)
 
 MSG_MAILGUN_TEST = emailify("""
