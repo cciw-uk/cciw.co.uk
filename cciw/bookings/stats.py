7dc3c8005c4e6168f45f4e334f062c58d454c7b1
Luke Plant <L.Plant.98@cantab.net>
2017-02-22 21:25:33 +0300

    Improved on pandas.fillna for booking progress chart.
    It was especially confusing for years that were not finished yet,
    but displayed lines to the end of the year

--- cciw/bookings/stats.py
+++ cciw/bookings/stats.py
@@ -1,5 +1,6 @@
-import datedelta
+from datetime import timedelta
 
+import datedelta
 import pandas as pd
 from django.db import models
 
@@ -32,12 +33,41 @@ def get_booking_progress_stats(start_year=None, end_year=None, camps=None, overl
             else:
                 dates = [d1.date() for d1, d2 in rows2]
             label = labeller(item)
-            data_dates[label] = accumulate_dates(dates)
-            data_rel_days[label] = accumulate([(r[0].date() - r[1]).days for r in rows2])
+            data_dates[label] = _fill_gaps(accumulate_dates(dates))
+            data_rel_days[label] = _fill_gaps(accumulate([(r[0].date() - r[1]).days for r in rows2]))
 
-    df1 = pd.DataFrame(data=data_dates).fillna(method='ffill').fillna(0)
-    df2 = pd.DataFrame(data=data_rel_days).fillna(method='ffill').fillna(0)
+    df1 = pd.DataFrame(data=data_dates)
+    df2 = pd.DataFrame(data=data_rel_days)
     return df1, df2
+
+
+def _fill_gaps(series):
+    # pandas 'fillna' method not good enough for us - we want the current year
+    # not to be filled until the end of the chart, just missing in-between values
+    # to be filled.
+    last_idx = None
+    last_val = None
+    extra = []
+    for idx in series.index:
+        if last_idx is not None:
+            if isinstance(idx, pd.Timestamp):
+                current_dt = idx.date()
+                last_dt = last_idx.date()
+                missing_dates = [last_dt + timedelta(days=days)
+                                 for days in range(1, (current_dt - last_dt).days)]
+                extra.extend((pd.Timestamp(dt), last_val) for dt in missing_dates)
+            else:
+                missing_days = [last_idx + days
+                                for days in range(1, idx - last_idx)]
+                extra.extend((day, last_val) for day in missing_days)
+
+        last_idx = idx
+        last_val = series[idx]
+
+    for idx, val in extra:
+        series[idx] = val
+    series = series.sort_index()
+    return series
 
 
 def get_booking_summary_stats(start_year, end_year):
