67c4e19d97fd6541831125fc283a72a978ab6bdb
Luke Plant <L.Plant.98@cantab.net>
2017-03-04 00:28:19 +0300

    Clarified DBS.completed field in admin

--- cciw/officers/models.py
+++ cciw/officers/models.py
@@ -455,10 +455,11 @@ class DBSCheck(models.Model):
                                 on_delete=models.CASCADE,
                                 related_name='dbs_checks')
     dbs_number = models.CharField("Disclosure number", max_length=20)
-    completed = models.DateField("Date of issue")
     check_type = models.CharField("check type", max_length=20,
                                   choices=CHECK_TYPE_CHOICES,
                                   default=CHECK_TYPE_FORM)
+    completed = models.DateField("Date of issue/check",
+                                 help_text="For full forms, use the date of issue. For online checks, use the date of the check")
     requested_by = models.CharField(max_length=20, choices=REQUESTED_BY_CHOICES, default=REQUESTED_BY_UKNOWN)
     other_organisation = models.CharField(max_length=255, blank=True)
     registered_with_dbs_update = models.NullBooleanField("registered with DBS update service")
