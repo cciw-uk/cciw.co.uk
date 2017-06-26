429893d6946df828f6e0ba92a0edf29585d0b6e6
Luke Plant <L.Plant.98@cantab.net>
2017-06-26 21:21:47 +0300

    Make it possible to add User.contact_phone_number in admin

--- cciw/accounts/admin.py
+++ cciw/accounts/admin.py
@@ -2,4 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . models import User
 
-admin.site.register(User, UserAdmin)
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None,
+         {'fields': ('username', 'password')}),
+        ('Personal info',
+         {'fields': (
+             'first_name', 'last_name', 'email', 'contact_phone_number')
+          }),
+        ('Permissions',
+         {'fields': (
+             'is_active', 'is_staff', 'is_superuser',
+             'groups', 'user_permissions')
+          }),
+        ('Important dates',
+         {'fields': (
+             'last_login', 'date_joined')
+          }),
+    )
+
+
+admin.site.register(User, MyUserAdmin)
