6f606b136693f64c55f66ffecb88792134755142
Luke Plant <L.Plant.98@cantab.net>
2020-04-23 21:19:17 +0300

    Converted camps sites view to functions

--- cciw/cciwmain/views/sites.py
+++ cciw/cciwmain/views/sites.py
@@ -1,23 +1,17 @@
 from django.shortcuts import get_object_or_404
 
-from cciw.cciwmain.common import CciwBaseView
+from django.template.response import TemplateResponse
 from cciw.cciwmain.models import Site
 
 
-class SiteList(CciwBaseView):
-    metadata_title = "Camp sites"
-    template_name = 'cciw/sites/index.html'
-
-    def handle(self, request):
-        return self.render({'sites': Site.objects.all()})
-
-
-class SiteDetail(CciwBaseView):
-    template_name = 'cciw/sites/detail.html'
-
-    def handle(self, request, slug=None):
-        return self.render({'site': get_object_or_404(Site.objects.filter(slug_name=slug))})
+def index(request):
+    return TemplateResponse(request, 'cciw/sites/index.html', {
+        'title': 'Camp sites',
+        'sites': Site.objects.all(),
+    })
 
 
-index = SiteList.as_view()
-detail = SiteDetail.as_view()
+def detail(request, slug=None):
+    return TemplateResponse(request, 'cciw/sites/detail.html', {
+        'site': get_object_or_404(Site.objects.filter(slug_name=slug)),
+    })
