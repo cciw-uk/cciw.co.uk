980b3ff5cfa06b1505faf82682656c7f41b2a0ac
Luke Plant <L.Plant.98@cantab.net>
2020-12-18 16:29:14 +0300

    Fixed --show-browser pytest option

--- conftest.py
+++ conftest.py
@@ -4,8 +4,9 @@ SHOW_BROWSER = False
 
 
 def pytest_addoption(parser):
-    parser.addoption("--show-browser", action="store_true", default=False)
+    parser.addoption("--show-browser", action="store_true", default=False, help="Show web browser window")
 
 
 def pytest_configure(config):
-    os.environ['TESTS_SHOW_BROWSER'] = 'TRUE'
+    if config.option.show_browser:
+        os.environ['TESTS_SHOW_BROWSER'] = 'TRUE'
