065ddb4b26ea394bc4074a3978ca84197f93fafc
Luke Plant <L.Plant.98@cantab.net>
2021-03-19 08:44:06 +0300

    Rewrote contact us page tests using functest

--- cciw/contact_us/tests.py
+++ cciw/contact_us/tests.py
@@ -1,18 +1,17 @@
-from bs4 import BeautifulSoup
 from django.conf import settings
 from django.core import mail
 from django.urls import reverse
 
 from cciw.cciwmain.tests.base import BasicSetupMixin
 from cciw.sitecontent.models import HtmlChunk
-from cciw.utils.tests.base import TestBase
+from cciw.utils.tests.webtest import WebTestBase
 
 from .models import Message
 
 CONTACT_US_URL = reverse("cciw-contact_us-send")
 
 
-class ContactUsPage(BasicSetupMixin, TestBase):
+class ContactUsPage(BasicSetupMixin, WebTestBase):
 
     def setUp(self):
         super().setUp()
@@ -25,61 +24,69 @@ class ContactUsPage(BasicSetupMixin, TestBase):
         HtmlChunk.objects.create(name="contact_us_outro")
 
     def test_cant_send_without_email(self):
-        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
-                                                   email="",
-                                                   message="The Message",
-                                                   cx_0="PASSED",
-                                                   cx_1="PASSED",
-                                                   subject="website"))
+        self.get_url('cciw-contact_us-send')
+        self.fill({
+            '#id_name': "My Name",
+            '#id_email': "",
+            '#id_message': "The Message",
+            '#id_cx_0': "PASSED",
+            '#id_cx_1': "PASSED",
+        })
+        self.submit('input[type="submit"]')
         self.assertEqual(len(mail.outbox), 0)
         self.assertEqual(Message.objects.count(), 0)
 
     def test_cant_send_without_valid_email(self):
-        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
-                                                   email="invalidemail",
-                                                   message="The Message",
-                                                   cx_0="PASSED",
-                                                   cx_1="PASSED",
-                                                   subject="website"))
+        self.get_url('cciw-contact_us-send')
+        self.fill({
+            '#id_name': "My Name",
+            '#id_email': "invalidemail",
+            '#id_message': "The Message",
+            '#id_cx_0': "PASSED",
+            '#id_cx_1': "PASSED",
+        })
+        self.submit('input[type="submit"]')
         self.assertEqual(len(mail.outbox), 0)
         self.assertEqual(Message.objects.count(), 0)
 
     def test_cant_send_without_message(self):
-        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
-                                                   email="validemail@somewhere.com",
-                                                   message="",
-                                                   cx_0="PASSED",
-                                                   cx_1="PASSED",
-                                                   subject="website"))
+        self.get_url('cciw-contact_us-send')
+        self.fill({
+            '#id_name': "My Name",
+            '#id_email': "invalidemail",
+            '#id_message': "",
+            '#id_cx_0': "PASSED",
+            '#id_cx_1': "PASSED",
+        })
+        self.submit('input[type="submit"]')
         self.assertEqual(len(mail.outbox), 0)
         self.assertEqual(Message.objects.count(), 0)
 
-    def test_form_appears(self):
-        r = self.client.get(CONTACT_US_URL)
-        b = BeautifulSoup(r.content, "lxml")
-        self.assertNotEqual(b.find(name='input', attrs={'name': 'email'}), None)
-        self.assertNotEqual(b.find(name='input', attrs={'name': 'name'}), None)
-        self.assertNotEqual(b.find(name='textarea', attrs={'name': 'message'}), None)
-
     def test_send(self):
-        self.client.post(CONTACT_US_URL, data=dict(name="My Name",
-                                                   email="validemail@somewhere.com",
-                                                   message="The Message",
-                                                   cx_0="PASSED",
-                                                   cx_1="PASSED",
-                                                   subject="general"))
+        self.get_url('cciw-contact_us-send')
+        self.fill({
+            '#id_name': "My Name",
+            '#id_email': "validemail@example.com",
+            '#id_message': "The Message",
+            '#id_cx_0': "PASSED",
+            '#id_cx_1': "PASSED",
+        })
+        self.submit('input[type="submit"]')
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(Message.objects.count(), 1)
         self.assertEqual(mail.outbox[0].to, settings.EMAIL_RECIPIENTS["GENERAL_CONTACT"])
 
     def test_send_to_booking_secretary(self):
-        self.client.post(CONTACT_US_URL,
-                         data=dict(name="My Name",
-                                   email="validemail@somewhere.com",
-                                   message="The Message",
-                                   cx_0="PASSED",
-                                   cx_1="PASSED",
-                                   subject="bookings"))
+        self.get_url('cciw-contact_us-send')
+        self.fill({
+            '#id_name': "My Name",
+            '#id_email': "validemail@example.com",
+            '#id_message': "The Message",
+            '#id_cx_0': "PASSED",
+            '#id_cx_1': "PASSED",
+            '#id_subject': "bookings",
+        })
+        self.submit('input[type="submit"]')
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(sorted(mail.outbox[0].to),
                          sorted(settings.EMAIL_RECIPIENTS["BOOKING_SECRETARY"]))
