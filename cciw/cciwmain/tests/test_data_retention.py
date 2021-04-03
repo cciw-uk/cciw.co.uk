9cbd3b04631323b715e93d304fd7a0cdfd4850bc
Luke Plant <L.Plant.98@cantab.net>
2021-04-03 12:28:44 +0300

    Added missing test

--- cciw/cciwmain/tests/test_data_retention.py
+++ cciw/cciwmain/tests/test_data_retention.py
@@ -25,6 +25,10 @@ def test_parse_keep_years():
     assert parse_keep('3 years') == timedelta(days=3 * 365)
 
 
+def test_parse_keep_days():
+    assert parse_keep('4 days') == timedelta(days=4)
+
+
 def test_parse_keep_other():
     with pytest.raises(ValueError):
         parse_keep('abc 123')
