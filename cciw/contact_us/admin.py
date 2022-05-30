43b6ba728e558772b58a50830362e61a391383e8
Luke Plant <L.Plant.98@cantab.net>
2022-05-30 20:41:21 +0100

    Better admin for contact_us.Message

--- cciw/contact_us/admin.py
+++ cciw/contact_us/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.template.defaultfilters import linebreaksbr
+from django.urls.base import reverse
+from django.utils.html import format_html
 
 from cciw.contact_us.models import Message
 
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ["id", "email", "booking_account", "name", "created_at"]
+    def view(message):
+        url = reverse("cciw-contact_us-view", kwargs={"message_id": message.id})
+        return format_html(f"""<a href='{url}'>View {message.id}</a>""")
+
+    def message_formatted(message):
+        return linebreaksbr(message.message)
+
+    list_display = ["id", "email", "booking_account", "name", message_formatted, view, "created_at"]
     autocomplete_fields = ["booking_account"]
 
 
