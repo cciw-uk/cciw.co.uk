8debd7e208276c8dd2f7c181bdbb0bf105f2dd49
Luke Plant <L.Plant.98@cantab.net>
2023-03-17 15:57:23 +0000

    Improved admin of contact_us.Message

--- cciw/contact_us/admin.py
+++ cciw/contact_us/admin.py
@@ -29,9 +29,18 @@ def classify_with_bogofilter(modeladmin, request, queryset: QuerySet[Message]):
 
 
 class MessageAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("booking_account")
+
     def view(message):
         url = reverse("cciw-contact_us-view", kwargs={"message_id": message.id})
         return format_html(f"""<a href='{url}'>View {message.id}</a>""")
+
+    @admin.display(description="Booking account")
+    def booking_account(message):
+        if message.booking_account:
+            return format_html("{0}<br>{1}", message.booking_account.name, message.booking_account.email)
+        return ""
 
     @admin.display(description="Message")
     def message_formatted(message):
@@ -42,11 +51,12 @@ class MessageAdmin(admin.ModelAdmin):
         "id",
         "subject",
         "email",
-        "booking_account",
+        booking_account,
         "name",
         message_formatted,
         "spam_classification_manual",
         "spam_classification_bogofilter",
+        "created_at",
         view,
     ]
     autocomplete_fields = ["booking_account"]
