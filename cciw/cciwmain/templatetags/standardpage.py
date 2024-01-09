efad266d4dcbcbeff15c2aa2032a2b3dbf292558
Luke Plant <luke@lukeplant.me.uk>
2024-01-09 21:44:00 +0000

    Reimplemented htmlchunk template tag using more modern facilities
    Fixes #39

--- cciw/cciwmain/templatetags/standardpage.py
+++ cciw/cciwmain/templatetags/standardpage.py
@@ -3,39 +3,18 @@ from django import template
 from cciw.cciwmain.common import standard_subs
 from cciw.sitecontent.models import HtmlChunk
 
-
-class RenderHtmlChunk(template.Node):
-    def __init__(self, chunk_name, ignore_missing=False):
-        self.chunk_name = chunk_name
-        self.ignore_missing = ignore_missing
-
-    def render(self, context):
-        try:
-            chunk = HtmlChunk.objects.get(name=self.chunk_name)
-        except HtmlChunk.DoesNotExist:
-            if not self.ignore_missing:
-                raise
-            chunk = None
-        if chunk is None:
-            return ""
-        return chunk.render(context["request"])
-
-
-def do_htmlchunk(parser, token):
-    """
-    Renders an HtmlChunk. It takes a single argument,
-    the name of the HtmlChunk to find.
-
-    If 'ignoremissing' is passed as second argument, errors will be ignored.
-    """
-    bits = token.contents.split(" ")
-    if len(bits) == 2:
-        ignore_missing = False
-    else:
-        ignore_missing = bits[2] == "ignoremissing"
-    return RenderHtmlChunk(bits[1], ignore_missing=ignore_missing)
-
-
 register = template.Library()
 register.filter(standard_subs)
-register.tag("htmlchunk", do_htmlchunk)
+
+
+@register.simple_tag(takes_context=True)
+def htmlchunk(context, name, ignore_missing=False):
+    try:
+        chunk = HtmlChunk.objects.get(name=name)
+    except HtmlChunk.DoesNotExist:
+        if not ignore_missing:
+            raise
+        chunk = None
+    if chunk is None:
+        return ""
+    return chunk.render(context["request"])
