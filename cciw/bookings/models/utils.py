bd767995989ad4d62dec1b2696f5478d2ca5c52e
Luke Plant <luke@lukeplant.me.uk>
2025-11-17 20:27:13 +0000

    Type hints

--- cciw/bookings/models/utils.py
+++ cciw/bookings/models/utils.py
@@ -11,23 +11,23 @@ from cciw.cciwmain.models import Camp
 from .prices import are_prices_set_for_year
 
 
-def any_bookings_possible(year: int):
+def any_bookings_possible(year: int) -> bool:
     camps = Camp.objects.filter(year=year)
     return any(c.get_places_left().total > 0 and c.is_open_for_bookings for c in camps)
 
 
-def is_booking_open(year: int):
+def is_booking_open(year: int) -> bool:
     """
     When passed a given year, returns True if booking is open.
     """
     return are_prices_set_for_year(year)
 
 
-def is_booking_open_thisyear():
+def is_booking_open_thisyear() -> bool:
     return is_booking_open(common.get_thisyear())
 
 
-def most_recent_booking_year():
+def most_recent_booking_year() -> int | None:
     from .bookings import Booking
 
     booking = Booking.objects.booked().order_by("-camp__year").select_related("camp").first()
@@ -42,5 +42,5 @@ def get_early_bird_cutoff_date(year: int) -> datetime:
     return datetime(year, 5, 1, tzinfo=timezone.get_default_timezone())
 
 
-def early_bird_is_available(year: int, booked_at_date: datetime):
+def early_bird_is_available(year: int, booked_at_date: datetime) -> bool:
     return booked_at_date < get_early_bird_cutoff_date(year)
