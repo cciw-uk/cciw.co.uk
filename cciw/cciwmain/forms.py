0284193de47a6cb53ceae21ec6426b1c452bf307
Luke Plant <luke@lukeplant.me.uk>
2026-01-11 19:16:09 +0000

    Fix patterns for htmx form validation
    We must add `hx-preserve` for the initial render for it to work

--- cciw/cciwmain/forms.py
+++ cciw/cciwmain/forms.py
@@ -11,7 +11,12 @@ class CciwFormMixin:
     template_name_p_formrow = "cciw/forms/p_formrow.html"
     required_css_class = "required"
 
-    do_htmx_validation: bool = False  # Set to True in subclasses to use htmx validation
+    # Set to True in subclasses to use htmx validation.
+    # Note that this will trigger `hx-preserve`
+    # attributes, which is incompatible with some other
+    # htmx usages where we want to overwrite form data
+    # with HTML from the server.
+    do_htmx_validation: bool = False
 
     # Dictionary from field name to label to override normal labels easily
     label_overrides: dict = {}
@@ -26,7 +31,7 @@ class CciwFormMixin:
         }
 
 
-def render_single_form_field(form: Form, field_name: str, *, validation_only: bool):
+def render_single_form_field(form: Form, field_name: str):
     # Assumes form has CciwFormMixin as a base
     bound_field = form[field_name]
     label_text = form.label_overrides.get(field_name, None)
@@ -35,7 +40,6 @@ def render_single_form_field(form: Form, field_name: str, *, validation_only: bo
             "field": bound_field,
             "errors": form.error_class(bound_field.errors, renderer=form.renderer),
             "do_htmx_validation": form.should_do_htmx_validation(),
-            "validation_only": validation_only,
         }
         | ({"label_tag": bound_field.label_tag(contents=label_text)} if label_text else {}),
         template_name=form.template_name_p_formrow,
