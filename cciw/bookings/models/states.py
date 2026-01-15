8c45de6a966aeb668c8249e1fa264d1b06cf91a4
Luke Plant <luke@lukeplant.me.uk>
2026-01-15 11:30:22 +0000

    Add BookingState.CANCELLED_BOOKING_FEE_KEPT

--- cciw/bookings/models/states.py
+++ cciw/bookings/models/states.py
@@ -5,5 +5,6 @@ class BookingState(models.TextChoices):
     INFO_COMPLETE = "info_complete", "Information complete"
     BOOKED = "booked", "Booked"
     CANCELLED_DEPOSIT_KEPT = "cancelled_deposit_kept", "Cancelled - deposit kept (pre 2026 only)"
-    CANCELLED_HALF_REFUND = "cancelled_half_refund", "Cancelled - half refund (pre 2015 only)"
+    CANCELLED_BOOKING_FEE_KEPT = "cancelled_booking_fee_kept", "Cancelled - booking fee kept"
+    CANCELLED_HALF_REFUND = "cancelled_half_refund", "Cancelled - half refund"
     CANCELLED_FULL_REFUND = "cancelled_full_refund", "Cancelled - full refund"
